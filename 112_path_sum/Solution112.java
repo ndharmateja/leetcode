d63c5d63784133a3c2b62469b19e18b6ccc18e43
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 14:46:21 +0530

    Solve 112

---
+++ 112_path_sum/Solution112.java
@@ -0,0 +1,27 @@
+class Solution112 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
