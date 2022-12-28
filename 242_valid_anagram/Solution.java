8dfd1b65d1e784e3445bf22894d8a0ad5029d85e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 06:06:40 +0530

    Solve 242

--- 242_valid_anagram/Solution.java
+++ 242_valid_anagram/Solution.java
@@ -1,32 +1,22 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Arrays;
 
 class Solution {
 
-    private HashMap<Character, Long> getCharCounts(String s) {
-        HashMap<Character, Long> counts = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            counts.put(c, counts.getOrDefault(c, 0L) + 1);
-        }
-        return counts;
-    }
-
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
         if (s.equals(t))
             return true;
-        HashMap<Character, Long> sCounts = getCharCounts(s);
-        HashMap<Character, Long> tCounts = getCharCounts(t);
 
-        HashSet<Character> chars = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            chars.add(c);
-        }
-        for (char c : t.toCharArray()) {
-            chars.add(c);
-        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
 
-        for (char c : chars) {
-            if (sCounts.get(c) != tCounts.get(c))
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        for (int i = 0; i < sChars.length; i++) {
+            if (sChars[i] != tChars[i])
                 return false;
         }
 
