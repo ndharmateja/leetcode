7eb9bd52c697f2b153e8e0e17aaf0812017ec2ac
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 11:30:03 +0530

    Solve 322 java

---
+++ 322_coin_change/Solution.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+class Solution {
+    public int coinChange(int[] coins, int amount, HashMap<Integer, Integer> memo) {
+        if (amount == 0)
+            return 0;
+        if (!memo.containsKey(amount)) {
+            int minValue = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (amount < coin)
+                    continue;
+                int prevMinValue = coinChange(coins, amount - coin, memo);
+                if (prevMinValue < 0)
+                    continue;
+                if (prevMinValue < minValue)
+                    minValue = coinChange(coins, amount - coin, memo);
+            }
+            if (minValue == Integer.MAX_VALUE) {
+                memo.put(amount, -1);
+            } else {
+                memo.put(amount, minValue + 1);
+            }
+        }
+        return memo.get(amount);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        return coinChange(coins, amount, new HashMap<>());
+    }
+
+}
