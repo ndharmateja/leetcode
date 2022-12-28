a2bc78983cb8ab892abb305353cb54139bc56457
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 07:07:18 +0530

    Improve 347

--- 347_top_k_frequent_elements/Solution.java
+++ 347_top_k_frequent_elements/Solution.java
@@ -7,8 +7,9 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
 
+    // O(n log n) solution
+    private int[] solution1(int[] nums, int k) {
         // 1. Get counts of all elements
         Map<Integer, Integer> counts = new HashMap<>();
         for (int num : nums)
@@ -30,5 +31,48 @@ class Solution {
             output[i] = entries.get(i).getKey();
         }
         return output;
+    }
+
+    // Reference: https://www.youtube.com/watch?v=YPTqKIgVk-k
+    // O(n) solution
+    private int[] solution2(int[] nums, int k) {
+        // 1. Get counts of all elements
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int num : nums)
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+
+        // 2. Create an array such that at a particular index 'i'
+        // we store the list of elements with count 'i'
+        // the max size of this array is 'n + 1' where n - num of elements in nums
+        // Because count can be atmost 'n' (if all elements are same)
+        @SuppressWarnings("unchecked")
+        List<Integer>[] array = new List[nums.length + 1];
+        for (int key : counts.keySet()) {
+            int count = counts.get(key);
+            if (array[count] == null) {
+                array[count] = new ArrayList<>();
+            }
+            array[count].add(key);
+        }
+
+        // 3. Scan the above array from right to left and add 'k' elements
+        int[] output = new int[k];
+        int i = 0;
+        for (int j = array.length - 1; j >= 0; j--) {
+            List<Integer> elements = array[j];
+            if (elements == null)
+                continue;
+            for (int element : elements) {
+                // Stop if 'k' elements are added already
+                if (i == k)
+                    break;
+                output[i++] = element;
+            }
+        }
+        return output;
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        return solution2(nums, k);
     }
 }
