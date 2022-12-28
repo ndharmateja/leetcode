cf65f12caa4674c921aee9570856a1a9c0c775cc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 16:24:01 +0530

    Improve 9

--- 9_palindrome_number/Solution9.java
+++ 9_palindrome_number/Solution9.java
@@ -7,12 +7,30 @@ class Solution9 {
     }
 
     // Checks palindrome after converting to string
+    @SuppressWarnings("unused")
     private boolean solution1(int x) {
         String xString = Integer.toString(x);
         return isPalindrome(xString);
     }
 
+    private int reverseNumber(int x) {
+        // number of digits
+        int numDigits = (int) Math.floor(Math.log10(x) + 1);
+        int reversed = 0;
+        for (int i = 0; i < numDigits; i++) {
+            int unitsDigit = x % 10;
+            x = x / 10;
+            reversed += unitsDigit * ((int) Math.pow(10, numDigits - i - 1));
+        }
+        return reversed;
+    }
+
+    // Checks palindrome without converting to string
+    private boolean solution2(int x) {
+        return x == reverseNumber(x);
+    }
+
     public boolean isPalindrome(int x) {
-        return solution1(x);
+        return solution2(x);
     }
 }
