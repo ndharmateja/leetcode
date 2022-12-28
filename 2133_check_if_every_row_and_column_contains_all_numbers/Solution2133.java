b9d60d69827bc475ae7134b9f2fe42dc52326562
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 18:08:56 +0530

    Solve 2133

---
+++ 2133_check_if_every_row_and_column_contains_all_numbers/Solution2133.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution2133 {
+    public boolean checkValid(int[][] matrix) {
+        Set<Integer> set = new HashSet<>();
+        int n = matrix.length;
+        for (int row = 0; row < n; row++) {
+            set.clear();
+            for (int i = 0; i < n; i++) {
+                if (matrix[row][i] == '.')
+                    continue;
+                int value = matrix[row][i];
+                if (set.contains(value) || value < 1 || value > n) {
+                    return false;
+                } else {
+                    set.add(value);
+                }
+            }
+        }
+
+        set.clear();
+        for (int col = 0; col < n; col++) {
+            set.clear();
+            for (int i = 0; i < n; i++) {
+                if (matrix[i][col] == '.')
+                    continue;
+                int value = matrix[i][col];
+                if (set.contains(value) || value < 1 || value > n) {
+                    return false;
+                } else {
+                    set.add(value);
+                }
+            }
+        }
+
+        return true;
+    }
+}
