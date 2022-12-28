e607fdfb11bef908fbfb962fae61e142ce557177
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 06:02:57 +0530

    Solve 217

---
+++ 217_contains_duplicate/Solution.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            if (numSet.contains(num))
+                return true;
+            numSet.add(num);
+        }
+        return false;
+    }
+}
