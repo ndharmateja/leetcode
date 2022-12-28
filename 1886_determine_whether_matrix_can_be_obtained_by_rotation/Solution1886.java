e2320433365ef208518677bcae58486ebbab5984
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-28 12:19:40 +0530

    Solve 1886

---
+++ 1886_determine_whether_matrix_can_be_obtained_by_rotation/Solution1886.java
@@ -0,0 +1,38 @@
+class Solution1886 {
+
+    private int[][] rotateClockwise(int[][] arr) {
+        int n = arr.length;
+        int[][] out = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                out[i][j] = arr[n - j - 1][i];
+            }
+        }
+        return out;
+    }
+
+    private boolean areArraysEqual(int[][] arr1, int[][] arr2) {
+        int n = arr1.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr1[i][j] != arr2[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean findRotation(int[][] mat, int[][] target) {
+        if (areArraysEqual(mat, target))
+            return true;
+
+        int[][] temp = mat;
+        for (int i = 0; i < 3; i++) {
+            temp = rotateClockwise(temp);
+            if (areArraysEqual(temp, target))
+                return true;
+        }
+
+        return false;
+    }
+}
