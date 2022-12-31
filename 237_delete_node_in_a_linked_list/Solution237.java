7a73281dece786e07e9cf8da09f9aebdf1de06ae
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-12-31 13:44:53 +0530

    Solve 237

---
+++ 237_delete_node_in_a_linked_list/Solution237.java
@@ -0,0 +1,23 @@
+class Solution237 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
