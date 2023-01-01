01d09be9a4af6f84f5f17558cf7748ce593b2e03
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-01 07:50:57 +0530

    Solve 28

---
+++ 28_find_the_index_of_the_first_occurrence_in_a_string/Solution28.java
@@ -0,0 +1,17 @@
+@SuppressWarnings("unused")
+class Solution {
+
+    // improvement: Use Rabin-Karp's algorithm
+    private int solution2(String haystack, String needle) {
+        return -1;
+    }
+
+    // Using inbuilt library
+    private int solution1(String haystack, String needle) {
+        return haystack.indexOf(needle, 0);
+    }
+
+    public int strStr(String haystack, String needle) {
+        return solution1(haystack, needle);
+    }
+}
