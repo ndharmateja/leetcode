a9f021e8fc5cdb568fb53c3eb59bb233fed73280
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-01 07:47:27 +0530

    Solve 24

---
+++ 24_swap_nodes_in_pairs/Solution24.java
@@ -0,0 +1,49 @@
+class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = head.next;
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+
+        while (true) {
+            if (prev != null)
+                prev.next = next;
+            curr.next = next.next;
+            next.next = curr;
+
+            prev = curr;
+
+            if (curr.next == null)
+                break;
+            curr = curr.next;
+
+            if (curr.next == null)
+                break;
+            next = curr.next;
+        }
+
+        return newHead;
+    }
+}
