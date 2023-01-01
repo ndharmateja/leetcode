082fb4835d8cf9a4be3b1e36b8b6578717fe97ac
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-01 07:35:59 +0530

    Solve 1721

--- 1721_swapping_nodes_in_a_linked_list/Solution1721.java
+++ 1721_swapping_nodes_in_a_linked_list/Solution1721.java
@@ -19,6 +19,37 @@ class Solution1721 {
         }
     }
 
+    // Solution with multiple passes
+    // First pass to get size
+    // and two more passes to get the corresponding two nodes
+    // Then swap the values between those two nodes
+    private ListNode solution1(ListNode head, int k) {
+        int size = 0;
+        ListNode curr = head;
+
+        while (curr != null) {
+            size++;
+            curr = curr.next;
+        }
+
+        curr = head;
+        for (int i = 0; i < k - 1; i++) {
+            curr = curr.next;
+        }
+        ListNode node1 = curr;
+
+        curr = head;
+        for (int i = 0; i < (size - k + 1) - 1; i++) {
+            curr = curr.next;
+        }
+        ListNode node2 = curr;
+
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+
+        return head;
+    }
     public ListNode swapNodes(ListNode head, int k) {
         return null;
     }
