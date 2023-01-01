798d6a2f5bca55f266cbb103ac6b7faba2ffef8c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-01 10:33:08 +0530

    Solve 11

--- 11_container_with_most_water/Solution11.java
+++ 11_container_with_most_water/Solution11.java
@@ -1,3 +1,4 @@
+@SuppressWarnings("unused")
 class Solution11 {
 
     // Brute force solution - check all containers
@@ -12,8 +13,32 @@ class Solution11 {
         return maxArea;
     }
 
+    // O(n) solution
+    // Reference: https://youtu.be/UuiTKBwPgAo
+    // Initialize left and right pointers at start and end of array
+    // If current area greater than max area, update
+    // if height of left is less than height of right, increase left pointer
+    // else decrease right pointer
+    private int solution2(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int maxArea = 0;
+
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            maxArea = Math.max(maxArea, area);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return maxArea;
+    }
+
     public int maxArea(int[] height) {
-        return solution1(height);
+        return solution2(height);
     }
 
 }
