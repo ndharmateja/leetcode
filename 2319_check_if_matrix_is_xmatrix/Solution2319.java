8d56a91068e6f58e3ef7066eb9d78209d23800ea
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-03 14:58:52 +0530

    Solve 2319

---
+++ 2319_check_if_matrix_is_xmatrix/Solution2319.java
@@ -0,0 +1,17 @@
+class Solution2319 {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if ((i == j || i + j == n - 1)) {
+                    if (grid[i][j] == 0)
+                        return false;
+                } else {
+                    if (grid[i][j] != 0)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
