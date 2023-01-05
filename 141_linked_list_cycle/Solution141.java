be70ecfc1a605a3558567d62dc2366fc10bf71dd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-05 16:30:19 +0530

    Improve 141

--- 141_linked_list_cycle/Solution141.java
+++ 141_linked_list_cycle/Solution141.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
+@SuppressWarnings("unused")
 public class Solution141 {
     static class ListNode {
         int val;
@@ -13,7 +14,33 @@ public class Solution141 {
     }
 
     public boolean hasCycle(ListNode head) {
-        return solution1(head);
+        return solution2(head);
+    }
+
+    // O(n) time and O(1) space
+    // Reference: https://youtu.be/gBTe7lFR3vc?t=225
+    // floyd's Tortoise and Hare
+    // Using two pointers
+    // Proof (that slow and fast have to come to same node eventually):
+    // At some point both 'slow' and 'fast' pointers
+    // are going to be in the cycle (if it exists)
+    // let the 'slow' pointer be ahead of 'fast' by 'n' nodes
+    // After one iteration, 'slow' will move by 1 and 'fast' will move by 2
+    // => 'slow' will be ahead of 'fast' by n + 1 - 2 = 'n - 1' nodes
+    // So after 'n' iterations, they both point to the same node
+    // So even if 'slow' is ahead of 'fast' by 'n' nodes, it still runs in O(n)
+    private boolean solution2(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (fast == slow)
+                return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
     }
 
     // O(n) time and O(n) space
