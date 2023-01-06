6609faa23f0227540b182ca1f8a6077c3d66f3c9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-06 19:17:26 +0530

    Improve 138

--- 138_copy_list_with_random_pointer/Solution138.java
+++ 138_copy_list_with_random_pointer/Solution138.java
@@ -21,33 +21,26 @@ class Solution138 {
     // O(n) time and space
     // Use a hash map to store the mappings between the old
     // and newly created nodes in the first pass while creating
-    // the new list
+    // the nodes of new list
     // Repass the second time through the old list and assign the
-    // random to each node of new list using the nodes map
+    // random and next to each node of new list using the nodes map
     private Node solution1(Node head) {
         Map<Node, Node> nodeMap = new HashMap<>();
 
-        Node currOld = head;
-        Node dummy = new Node(-1);
-        Node currNew = dummy;
-        while (currOld != null) {
-            currNew.next = new Node(currOld.val);
-            nodeMap.put(currOld, currNew.next);
-            currOld = currOld.next;
-            currNew = currNew.next;
+        Node curr = head;
+        while (curr != null) {
+            nodeMap.put(curr, new Node(curr.val));
+            curr = curr.next;
         }
 
-        Node newHead = dummy.next;
-
-        currOld = head;
-        currNew = newHead;
-
-        while (currOld != null) {
-            currNew.random = nodeMap.get(currOld.random);
-            currNew = currNew.next;
-            currOld = currOld.next;
+        curr = head;
+        while (curr != null) {
+            Node currNew = nodeMap.get(curr);
+            currNew.random = nodeMap.get(curr.random);
+            currNew.next = nodeMap.get(curr.next);
+            curr = curr.next;
         }
 
-        return newHead;
+        return nodeMap.get(head);
     }
 }
