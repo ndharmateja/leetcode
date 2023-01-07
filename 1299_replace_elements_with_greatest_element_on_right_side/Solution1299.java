df42e66d4d78619971adb1069c06af7024800d87
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-07 23:55:18 +0530

    Solve 1299

---
+++ 1299_replace_elements_with_greatest_element_on_right_side/Solution1299.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int currMax = arr[arr.length - 1];
+        arr[arr.length - 1] = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            int val = arr[i];
+            arr[i] = currMax;
+            if (val > currMax)
+                currMax = val;
+        }
+        return arr;
+    }
+}
