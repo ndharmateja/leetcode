b0a0b59b45ba5127f64d3c95c6bccdb6dfe41012
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-08 00:56:46 +0530

    Make DoublyLinkedList generic

--- data_structures/DoublyLinkedList.java
+++ data_structures/DoublyLinkedList.java
@@ -1,35 +1,35 @@
 package data_structures;
 
-public class DoublyLinkedList {
-    static class Node {
-        int val;
-        Node prev;
-        Node next;
+public class DoublyLinkedList<T> {
+    static class Node<T> {
+        T val;
+        Node<T> prev;
+        Node<T> next;
 
-        Node(int val) {
+        Node(T val) {
             this.val = val;
         }
     }
 
     // Sentinel nodes
-    Node head;
-    Node tail;
+    Node<T> head;
+    Node<T> tail;
     int size;
 
     public DoublyLinkedList() {
         this.size = 0;
-        this.head = new Node(0);
-        this.tail = new Node(0);
+        this.head = new Node<>(null);
+        this.tail = new Node<>(null);
         this.head.next = this.tail;
         this.tail.prev = this.head;
     }
 
-    public int get(int index) {
+    public T get(int index) {
         if (index < 0 || index >= size) {
-            return -1;
+            return null;
         }
 
-        Node curr = this.head.next;
+        Node<T> curr = this.head.next;
         for (int i = 0; i < index; i++) {
             curr = curr.next;
         }
@@ -37,8 +37,8 @@ public class DoublyLinkedList {
         return curr.val;
     }
 
-    public void addAtHead(int val) {
-        Node newNode = new Node(val);
+    public void addAtHead(T val) {
+        Node<T> newNode = new Node<>(val);
         newNode.next = this.head.next;
         this.head.next.prev = newNode;
         this.head.next = newNode;
@@ -46,8 +46,8 @@ public class DoublyLinkedList {
         this.size++;
     }
 
-    public void addAtTail(int val) {
-        Node newNode = new Node(val);
+    public void addAtTail(T val) {
+        Node<T> newNode = new Node<>(val);
         newNode.prev = this.tail.prev;
         this.tail.prev.next = newNode;
         this.tail.prev = newNode;
@@ -55,18 +55,18 @@ public class DoublyLinkedList {
         this.size++;
     }
 
-    public void addAtIndex(int index, int val) {
+    public void addAtIndex(int index, T val) {
         if (index < 0 || index > size) {
             return;
         }
 
-        Node curr = this.head;
+        Node<T> curr = this.head;
         for (int i = 0; i < index; i++) {
             curr = curr.next;
         }
 
         // Insert new node after curr
-        Node newNode = new Node(val);
+        Node<T> newNode = new Node<>(val);
         newNode.next = curr.next;
         newNode.next.prev = newNode;
         newNode.prev = curr;
@@ -80,7 +80,7 @@ public class DoublyLinkedList {
             return;
         }
 
-        Node curr = this.head.next;
+        Node<T> curr = this.head.next;
         for (int i = 0; i < index; i++) {
             curr = curr.next;
         }
@@ -100,16 +100,25 @@ public class DoublyLinkedList {
 
         StringBuilder builder = new StringBuilder();
         builder.append('[');
-        Node curr = this.head;
+        Node<T> curr = this.head;
         for (int i = 0; i < this.size; i++) {
             curr = curr.next;
-            builder.append(curr.val);
+            builder.append(curr.val.toString());
             builder.append(" ⇌ ");
         }
 
-        builder.setLength(builder.length() - 4);
+        builder.setLength(builder.length() - 3);
         builder.append(']');
 
         return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList<String> dll = new DoublyLinkedList<>();
+        dll.addAtHead("dharma");
+        dll.addAtHead("teja");
+        System.out.println(dll);
+        System.out.println(dll.get(1));
+        System.out.println(dll.get(2));
     }
 }
