c0d23d76736877763445a642afcc0c9995f7b9a0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-09 11:26:55 +0530

    Solve 796

---
+++ 796_rotate_string/Solution796.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution796 {
+    private Map<Character, Integer> getCharCounts(String string) {
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : string.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+        return counts;
+    }
+
+    private String rotateOnce(String s) {
+        return s.substring(1) + s.charAt(0);
+    }
+
+    // O(n^2) time and O(n^2) space solution
+    public boolean solution1(String s, String goal) {
+        if (s.equals(goal))
+            return true;
+        if (s.length() != goal.length())
+            return false;
+        if (!getCharCounts(s).equals(getCharCounts(goal)))
+            return false;
+
+        // Rotate string once each time and compare with goal
+        for (int i = 0; i < s.length() - 1; i++) {
+            s = rotateOnce(s);
+            if (s.equals(goal))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean rotateString(String s, String goal) {
+        return solution1(s, goal);
+    }
+}
