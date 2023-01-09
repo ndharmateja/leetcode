ea59b191a57ec1e29f8f50f4cbc1fa0bed5cee5c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-09 11:33:34 +0530

    Improve 796

--- 796_rotate_string/Solution796.java
+++ 796_rotate_string/Solution796.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 class Solution796 {
     private Map<Character, Integer> getCharCounts(String string) {
         Map<Character, Integer> counts = new HashMap<>();
@@ -14,7 +15,7 @@ class Solution796 {
         return s.substring(1) + s.charAt(0);
     }
 
-    // O(n^2) time and O(n^2) space solution
+    // O(n) time and O(n^2) space solution
     public boolean solution1(String s, String goal) {
         if (s.equals(goal))
             return true;
@@ -30,6 +31,11 @@ class Solution796 {
                 return true;
         }
         return false;
+    }
+
+    // O(n) time and O(n) space solution
+    public boolean solution2(String s, String goal) {
+        return s.length() == goal.length() && (s + s).contains(goal);
     }
 
     public boolean rotateString(String s, String goal) {
