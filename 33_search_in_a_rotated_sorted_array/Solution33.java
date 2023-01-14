86cf557305a58eac3bf10a37c22dc4593539f87c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-14 16:04:42 +0530

    Solve 33

--- 33_search_in_a_rotated_sorted_array/Solution33.java
+++ 33_search_in_a_rotated_sorted_array/Solution33.java
@@ -1,4 +1,13 @@
 class Solution33 {
+    // O(n) time and O(1) space solution
+    // Linear Search
+    private int solution1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+        return -1;
+    }
     public int search(int[] nums, int target) {
         return -1;
     }
