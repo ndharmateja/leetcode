564a7f3d7c492a5312b5bd6958154555f225f1bc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-16 10:08:09 +0530

    Improve 125

--- 125_valid_palindrome/Solution125.java
+++ 125_valid_palindrome/Solution125.java
@@ -1,7 +1,29 @@
+@SuppressWarnings("unused")
 class Solution125 {
 
     public boolean isPalindrome(String s) {
-        return solution1(s);
+        return solution2(s);
+    }
+
+    // O(n) time and O(1) space solution
+    // Using left and right pointers
+    private boolean solution2(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            if (!Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(l++)) != Character.toLowerCase(s.charAt(r--)))
+                return false;
+        }
+
+        return true;
     }
 
     // O(n) time and O(n) space solution
