394a71fbc5282965d8174063a9106cf26f2b189a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-19 23:18:49 +0530

    Improve 560

--- 560_subarray_sum_equals_k/Solution560.java
+++ 560_subarray_sum_equals_k/Solution560.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 class Solution560 {
     public int subarraySum(int[] nums, int k) {
-        return solution1(nums, k);
+        return solution2(nums, k);
     }
 
     // O(n^2) time and O(1) space solution
@@ -35,6 +37,38 @@ class Solution560 {
                 if (nums[i] - nums[j] == k)
                     count++;
             }
+        }
+
+        return count;
+    }
+
+    // O(n) time and O(n) space solution
+    // Reference: https://youtu.be/fFVZt-6sgyo
+    private int solution2(int[] nums, int k) {
+        // Hash map for prefix sum counts
+        Map<Integer, Integer> prefixSumCounts = new HashMap<>();
+
+        // We add a count of '1' for '0'
+        // to account for the fact if the currPrefixSum itself
+        // is equal to 'k', then we will have a value of '1'
+        // for (currPrefixSum - k = 0)
+        prefixSumCounts.put(0, 1);
+
+        // Count variable for storing total counts
+        int count = 0;
+        int currPrefixSum = 0;
+
+        // Compute prefix sums and store them in nums itself
+        for (int num : nums) {
+            // Get count of all complement prefix sums (k - currPrefixSum)
+            // from map and add to count
+            // Since a subarray sum is the difference in the prefix sums
+            currPrefixSum += num;
+            int complementSum = currPrefixSum - k;
+            count += prefixSumCounts.getOrDefault(complementSum, 0);
+
+            // Put current prefix sum in the map
+            prefixSumCounts.put(currPrefixSum, prefixSumCounts.getOrDefault(currPrefixSum, 0) + 1);
         }
 
         return count;
