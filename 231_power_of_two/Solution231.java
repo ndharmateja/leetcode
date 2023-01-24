f836b2c01d1f9f9992da43a9c3eeb7bdfb07c781
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-24 05:59:40 +0530

    Fix bug in 231

--- 231_power_of_two/Solution231.java
+++ 231_power_of_two/Solution231.java
@@ -4,15 +4,15 @@ class Solution231 {
         return solution1(n);
     }
 
-    // O(log n) time and O(log n) space (recursion) solution
+    // O(log n) time and O(log n) space (recursion stack) solution
     // Using recursion
     private boolean solution1(int n) {
         if (n == 0)
             return false;
-        return ((n == 1) || (n % 2 == 0 && isPowerOfTwo(n / 2)));
+        return ((n == 1) || (n % 2 == 0 && solution1(n / 2)));
     }
 
-    // O(log n) time and O(1) space (recursion) solution
+    // O(log n) time and O(1) space solution
     // Using loops
     private boolean solution2(int n) {
         if (n == 0)
