f784eff6123247f9618a5b36d64647acdae785dc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-24 05:58:20 +0530

    Solve 231

---
+++ 231_power_of_two/Solution231.java
@@ -0,0 +1,24 @@
+@SuppressWarnings("unused")
+class Solution231 {
+    public boolean isPowerOfTwo(int n) {
+        return solution1(n);
+    }
+
+    // O(log n) time and O(log n) space (recursion) solution
+    // Using recursion
+    private boolean solution1(int n) {
+        if (n == 0)
+            return false;
+        return ((n == 1) || (n % 2 == 0 && isPowerOfTwo(n / 2)));
+    }
+
+    // O(log n) time and O(1) space (recursion) solution
+    // Using loops
+    private boolean solution2(int n) {
+        if (n == 0)
+            return false;
+        while (n % 2 == 0)
+            n /= 2;
+        return n == 1;
+    }
+}
