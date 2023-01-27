45607e481b810c5de608481231c5ce4e97763313
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-27 13:00:48 +0530

    Solve 657

---
+++ 657_robot_return_to_origin/Solution657.java
@@ -0,0 +1,16 @@
+class Solution657 {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'L')
+                x--;
+            else if (c == 'R')
+                x++;
+            else if (c == 'U')
+                y++;
+            else if (c == 'D')
+                y--;
+        }
+        return x == 0 && y == 0;
+    }
+}
