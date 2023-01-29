e32c243a566d26f5258955680cbec05234179b29
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-29 06:38:24 +0530

    Add comments for 1807

--- 1807_evaluate_bracket_pairs_of_a_string/Solution1807.java
+++ 1807_evaluate_bracket_pairs_of_a_string/Solution1807.java
@@ -3,7 +3,9 @@ import java.util.List;
 import java.util.Map;
 
 class Solution1807 {
+    // O(n) time and O(n) space solution
     public String evaluate(String s, List<List<String>> knowledge) {
+        // Put all key values in a map
         Map<String, String> map = new HashMap<>();
         for (List<String> keyValue : knowledge) {
             map.put(keyValue.get(0), keyValue.get(1));
@@ -14,15 +16,27 @@ class Solution1807 {
         boolean isBracketPair = false;
 
         for (char c : s.toCharArray()) {
+            // If opening bracket and isBracketPair is false
+            // we make the boolean true
             if (c == '(' && !isBracketPair) {
                 isBracketPair = true;
-            } else if (c == ')' && isBracketPair) {
+            }
+            // If closing bracket and isBracketPair is true
+            // we add the word (with value from map) to our builder
+            // and make the boolean false and reset word
+            else if (c == ')' && isBracketPair) {
                 builder.append(map.getOrDefault(word.toString(), "?"));
                 word.setLength(0);
                 isBracketPair = false;
-            } else if (isBracketPair) {
+            }
+            // if it is part of a bracket pair
+            // we add it to the word
+            else if (isBracketPair) {
                 word.append(c);
-            } else if (!isBracketPair) {
+            }
+            // if it is not a bracket pair
+            // we add it to our builder
+            else if (!isBracketPair) {
                 builder.append(c);
             }
         }
