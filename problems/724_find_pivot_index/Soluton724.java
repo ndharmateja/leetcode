9756b556fa43fee6cc20ca42ba5e6f5519303c7c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-01-31 11:05:55 +0530

    Solve 724

---
+++ problems/724_find_pivot_index/Soluton724.java
@@ -0,0 +1,30 @@
+class Solution724 {
+    public int pivotIndex(int[] nums) {
+        if (nums.length == 1)
+            return 0;
+
+        int[] prefixSums = new int[nums.length];
+        int[] postfixSums = new int[nums.length];
+
+        prefixSums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixSums[i] = prefixSums[i - 1] + nums[i];
+        }
+
+        postfixSums[nums.length - 1] = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            postfixSums[i] = postfixSums[i + 1] + nums[i];
+        }
+
+        if (postfixSums[1] == 0)
+            return 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (prefixSums[i - 1] == postfixSums[i + 1])
+                return i;
+        }
+        if (prefixSums[nums.length - 2] == 0)
+            return nums.length - 1;
+
+        return -1;
+    }
+}
