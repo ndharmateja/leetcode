4e1ab04937db0a9c504e98f782fab36a90b68368
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-02-26 09:34:49 +0530

    572: Attempt

---
+++ problems/572_subtree_of_another_tree/Solution572.java
@@ -0,0 +1,26 @@
+class Solution572 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null)
+            return false;
+        return root == subRoot || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+}
