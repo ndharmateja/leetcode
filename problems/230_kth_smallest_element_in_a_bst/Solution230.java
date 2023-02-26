f0b9c93ec6c2eccdd3d6874fbdcb742d2304821b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-02-26 13:32:04 +0530

    230: Attempt

---
+++ problems/230_kth_smallest_element_in_a_bst/Solution230.java
@@ -0,0 +1,44 @@
+class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class MutableInteger {
+        int value;
+
+        public MutableInteger(int value) {
+            this.value = value;
+        }
+
+        void increment() {
+            this.value++;
+        }
+
+        int getValue() {
+            return this.value;
+        }
+    }
+
+    private int solution1(TreeNode root, int k) {
+        return -1;
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        return solution1(root, k);
+    }
+}
