ec219faac0f13aab00f9b5e670c79baf87e4e39b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-03-20 09:20:01 +0530

    572: Solve

--- problems/572_subtree_of_another_tree/Solution572.java
+++ problems/572_subtree_of_another_tree/Solution572.java
@@ -18,9 +18,37 @@ class Solution572 {
         }
     }
 
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null)
+    private boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null)
+            return true;
+        if (root == null || subRoot == null)
             return false;
-        return root == subRoot || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        return root.val == subRoot.val && isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null)
+            return true;
+        if (root == null || subRoot == null)
+            return false;
+        if (isSameTree(root, subRoot))
+            return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.right.left = new TreeNode(2);
+
+        TreeNode subRoot = new TreeNode(3);
+        subRoot.left = new TreeNode(1);
+        subRoot.left = new TreeNode(2);
+
+        boolean ans = new Solution572().isSubtree(root, subRoot);
+        System.out.println(ans);
     }
 }
