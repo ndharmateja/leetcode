06a10527cb9445cfdafbb70dab1a616e46e55b7f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-06-13 17:29:36 +0530

    49: improve

--- problems/49_group_anagrams/Solution49.java
+++ problems/49_group_anagrams/Solution49.java
@@ -3,19 +3,31 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+@SuppressWarnings("unused")
 class Solution49 {
-
-    private String sortString(String str) {
+    // Key of a string is its sorted chars array string
+    // which will be same for all the anagrams
+    private String getKey2(String str) {
         char[] chars = str.toCharArray();
         Arrays.sort(chars);
-        return new String(chars);
+        return Arrays.toString(chars);
+    }
+
+    // Key of a string is the counts array string
+    // which will be same for all the anagrams
+    private String getKey1(String str, int[] counts) {
+        Arrays.fill(counts, 0);
+        for (char c : str.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        return Arrays.toString(counts);
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
-
+        int[] counts = new int[26];
         for (String str : strs) {
-            String key = sortString(str);
+            String key = getKey1(str, counts);
             List<String> anagramGroup = map.getOrDefault(key, new ArrayList<>());
             anagramGroup.add(str);
             map.put(key, anagramGroup);
