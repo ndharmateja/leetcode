52a1ba50bff53613deba32edcb8b6fa29dda26da
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-06-15 08:10:47 +0530

    36: improve

--- problems/36_valid_sudoku/Solution36.java
+++ problems/36_valid_sudoku/Solution36.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Solution36 {
-    public boolean isValidSudoku(char[][] board) {
+    // O(3n^2) time and O(n) space (n is 9)
+    public boolean solution1(char[][] board) {
         Set<Integer> set = new HashSet<>();
         for (int row = 0; row < 9; row++) {
             set.clear();
@@ -55,4 +58,40 @@ public class Solution36 {
         return true;
     }
 
+    // O(n^2) time and O(3 * n * n) space
+    public boolean solution2(char[][] board) {
+        // three hash maps for rows, columns and squares
+        // to check duplicates
+        Map<Integer, Set<Character>> rowMap = new HashMap<>();
+        Map<Integer, Set<Character>> colMap = new HashMap<>();
+        Map<Integer, Set<Character>> squareMap = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            rowMap.put(i, new HashSet<>());
+            colMap.put(i, new HashSet<>());
+            squareMap.put(i, new HashSet<>());
+        }
+
+        // iterate through all elements and see if it exists in the corresponding sets
+        // of the three maps
+        // if not add them
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board.length; c++) {
+                char element = board[r][c];
+                if (element == '.')
+                    continue;
+
+                int squareId = (r / 3) * 3 + (c / 3);
+                if (!rowMap.get(r).add(element) || !colMap.get(c).add(element)
+                        || !squareMap.get(squareId).add(element))
+                    return false;
+            }
+        }
+
+        // valid sudoku
+        return true;
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        return solution2(board);
+    }
 }
