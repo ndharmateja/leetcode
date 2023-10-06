9708c308b14542cb78b41da13c6a27b5e73647bf
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-06 20:27:05 +0530

    210: improve
    - add dfs implementation

--- problems/210_course_schedule_2/Solution210.java
+++ problems/210_course_schedule_2/Solution210.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
+@SuppressWarnings({ "unchecked", "unused" })
 class Solution210 {
     static class Vertex {
         int id;
@@ -23,11 +26,102 @@ class Solution210 {
         }
     }
 
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        return solution1(numCourses, prerequisites);
+    }
+
+    // O(|V| + |E|) time
+    // Standard DFS
+    private int[] solution2(int numCourses, int[][] prerequisites) {
+        // Build the adjacency list
+        ArrayList<Integer>[] adj = buildAdjacencyList(numCourses, prerequisites);
+
+        // Initialize sets for visited and current path
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> currentPath = new HashSet<>();
+        ArrayList<Integer> topologicalOrder = new ArrayList<>();
+
+        // For each vertex in the graph, explore it if it is unvisited
+        for (int v = 0; v < adj.length; v++) {
+            if (!visited.contains(v)) {
+                // If explore call returns true, we can return false
+                // as it contains a cycle
+                // and we don't need to explore the remaining vertices
+                boolean containsCycle = explore(v, adj, visited, currentPath, topologicalOrder);
+                if (containsCycle) {
+                    return new int[0];
+                }
+            }
+            // Once a explore call is done completely
+            // we don't need to reset current path as it automatically gets cleared
+        }
+
+        // If we reach here it means that there is no cycle
+        // so we can return the course order
+        // Reverse the order of topological and put in an array
+        int[] courseOrder = new int[numCourses];
+        for (int i = 0; i < courseOrder.length; i++) {
+            courseOrder[i] = topologicalOrder.get(numCourses - i - 1);
+        }
+        return courseOrder;
+    }
+
+    private ArrayList<Integer>[] buildAdjacencyList(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[numCourses];
+        for (int i = 0; i < adj.length; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int[] prerequisite : prerequisites) {
+            adj[prerequisite[1]].add(prerequisite[0]);
+        }
+        return adj;
+    }
+
+    // Recursively explores the vertex v through DFS
+    // Returns true if there is a cycle along this exploration
+    // Returns false otherwise
+    private boolean explore(int v, ArrayList<Integer>[] adj, Set<Integer> visited, Set<Integer> currentPath,
+            ArrayList<Integer> topologicalOrder) {
+        // Add the vertex to the visited and the current path sets
+        visited.add(v);
+        currentPath.add(v);
+
+        // Explore each unvisited neighbour
+        for (int neighbour : adj[v]) {
+            // If the neighbour is already along the current path
+            // we return true
+            if (currentPath.contains(neighbour)) {
+                return true;
+            }
+
+            // Explore each unvisited neighbour
+            if (!visited.contains(neighbour)) {
+                // If exploring a neighbour contains a cycle (call returns true)
+                // we can return true and we don't need to explore the remaining neighbours
+                boolean containsCycle = explore(neighbour, adj, visited, currentPath, topologicalOrder);
+                if (containsCycle) {
+                    return true;
+                }
+            }
+        }
+
+        // Remove v from the current path as its exploration is done
+        // and add the current vertex to the course order
+        // The highest post number vertices will be sources
+        // so the vertices will be added in the reverse topological order
+        // as sinks are added first and sinks are added last
+        currentPath.remove(v);
+        topologicalOrder.add(v);
+
+        // Return false as no cycle has been found in neighbours
+        return false;
+    }
+
     // Topological sort
     // O(|V| + |E|) time
     // O(|E|) to build the graph by iterating over all the edges
     // O(|V|) to iterate over all the vertices to remove them one by one
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    public int[] solution1(int numCourses, int[][] prerequisites) {
         // Build map of vertices
         // key: vertex id
         // value: vertex object
