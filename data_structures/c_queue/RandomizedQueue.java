d5e50edae6daeb816165f3389d0225f49a2184a5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-23 07:39:09 +0530

    Abstract RandomizedArrayIterator

--- data_structures/c_queue/RandomizedQueue.java
+++ data_structures/c_queue/RandomizedQueue.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
+import data_structures.iterators.RandomizedArrayIterator;
+
 @SuppressWarnings("unchecked")
 public class RandomizedQueue<T> implements Queue<T> {
     private T[] array;
@@ -114,66 +116,7 @@ public class RandomizedQueue<T> implements Queue<T> {
     // return an independent iterator over items in random order
     // Each iterator independently takes memory proportional to num items
     public Iterator<T> iterator() {
-        return new ArrayIterator();
-    }
-
-    private class ArrayIterator implements Iterator<T> {
-        T[] iteratorArray;
-        int curr = 0;
-
-        public ArrayIterator() {
-            // Create an copy array with the size of "size"
-            // to store all the elements and copy all elements
-            // from the randomized queue to it
-            iteratorArray = (T[]) new Object[size];
-            for (int i = 0; i < size; i++) {
-                int index = (i + head) % array.length;
-                iteratorArray[i] = array[index];
-            }
-
-            // Randomly shuffle it
-            shuffle(iteratorArray);
-        }
-
-        // Fisher Yates shuffle
-        // Ref:
-        // https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
-        private void shuffle(T[] arr) {
-            int n = arr.length - 1;
-            Random r = new Random();
-
-            // Start from the last element and swap one by one. We don't
-            // need to run for the first element that's why i > 0
-            for (int i = n - 1; i > 0; i--) {
-
-                // Pick a random index from 0 to i
-                int j = r.nextInt(i + 1);
-
-                // Swap arr[i] with the element at random index
-                T temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        @Override
-        public boolean hasNext() {
-            return curr < iteratorArray.length;
-        }
-
-        @Override
-        public T next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException("Reached the end of iterator");
-            }
-
-            return iteratorArray[curr++];
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
+        return new RandomizedArrayIterator<>(array, head, size);
     }
 
     // unit testing (required)
---
+++ data_structures/iterators/RandomizedArrayIterator.java
@@ -0,0 +1,76 @@
+package data_structures.iterators;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+public class RandomizedArrayIterator<T> implements Iterator<T> {
+    T[] iteratorArray;
+    int curr = 0;
+
+    /**
+     * Creates a random iterator of the numElements elements
+     * starting from head index in the forward direction (circularly iterates from
+     * the start once the end is reached)
+     * 
+     * Note: This creates an independent copy of the required elements
+     * 
+     * @param array
+     * @param head
+     * @param numElements
+     */
+    @SuppressWarnings("unchecked")
+    public RandomizedArrayIterator(T[] array, int head, int numElements) {
+        // Create an copy array with the size of "size"
+        // to store all the elements and copy all elements
+        // from the randomized queue to it
+        iteratorArray = (T[]) new Object[numElements];
+        for (int i = 0; i < numElements; i++) {
+            int index = (i + head) % array.length;
+            iteratorArray[i] = array[index];
+        }
+
+        // Randomly shuffle it
+        shuffle(iteratorArray);
+    }
+
+    // Fisher Yates shuffle
+    // Ref:
+    // https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+    private void shuffle(T[] arr) {
+        int n = arr.length - 1;
+        Random r = new Random();
+
+        // Start from the last element and swap one by one. We don't
+        // need to run for the first element that's why i > 0
+        for (int i = n - 1; i > 0; i--) {
+
+            // Pick a random index from 0 to i
+            int j = r.nextInt(i + 1);
+
+            // Swap arr[i] with the element at random index
+            T temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return curr < iteratorArray.length;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("Reached the end of iterator");
+        }
+
+        return iteratorArray[curr++];
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
