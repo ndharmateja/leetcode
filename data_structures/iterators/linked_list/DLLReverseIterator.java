55e4f89385c3f7781db02902a69b97b9e43f4444
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-23 07:44:20 +0530

    Add DLLReverseIterator

---
+++ data_structures/iterators/linked_list/DLLReverseIterator.java
@@ -0,0 +1,42 @@
+package data_structures.iterators.linked_list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import data_structures.nodes.linked_list.DLLNode;
+
+public class DLLReverseIterator<T> implements Iterator<T> {
+    private DLLNode<T> curr;
+    private DLLNode<T> head;
+
+    /**
+     * Reverse iterator for a doubly linked list
+     * with sentinel nodes head and tail
+     * 
+     * @param head
+     * @param tail
+     */
+    public DLLReverseIterator(DLLNode<T> head, DLLNode<T> tail) {
+        curr = tail.prev;
+        this.head = head;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return curr != head;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        T t = curr.data;
+        curr = curr.prev;
+        return t;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
