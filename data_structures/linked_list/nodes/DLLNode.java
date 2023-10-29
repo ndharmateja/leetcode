64d824f6aeceae394d8677b7c8314b3eeaf49f7c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-29 14:26:08 +0530

    Remove data structures folder

--- data_structures/linked_list/nodes/DLLNode.java
+++
@@ -1,7 +0,0 @@
-package data_structures.linked_list.nodes;
-
-public class DLLNode<T> {
-    public T data;
-    public DLLNode<T> next;
-    public DLLNode<T> prev;
-}
--- data_structures/linked_list/nodes/SLLNode.java
+++
@@ -1,6 +0,0 @@
-package data_structures.linked_list.nodes;
-
-public class SLLNode<T> {
-    public T data;
-    public SLLNode<T> next;
-}
