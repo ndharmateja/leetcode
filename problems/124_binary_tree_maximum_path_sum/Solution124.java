f38e116226f5cb0635be43bf0edda78207dbf1e9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-12-10 00:54:06 +0530

    124: init

---
+++ problems/124_binary_tree_maximum_path_sum/Solution124.java
@@ -0,0 +1,24 @@
+public class Solution124 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxPathSum(TreeNode root) {
+        return -1;
+    }
+}
