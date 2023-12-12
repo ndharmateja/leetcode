6ee2f3ffc46ebe83b2355970a093e59446e4cf11
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-12-13 02:01:13 +0530

    56: add edge case

--- problems/56_merge_intervals/Solution56.java
+++ problems/56_merge_intervals/Solution56.java
@@ -3,6 +3,11 @@ import java.util.PriorityQueue;
 
 public class Solution56 {
     public int[][] merge(int[][] intervals) {
+        // Edge case - only one interval
+        // we return intervals itself
+        if (intervals.length <= 1)
+            return intervals;
+
         // Priority queue so that we get intervals in increasing order
         // of starts of intervals
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
@@ -10,7 +15,7 @@ public class Solution56 {
             pq.add(interval);
 
         ArrayList<int[]> mergedList = new ArrayList<>();
-        int[] currMerged = pq.peek();
+        int[] currMerged = pq.poll();
         while (!pq.isEmpty()) {
             int[] currInterval = pq.poll();
             // If currInterval has an intersection with currMerged
