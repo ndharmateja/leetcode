139affff70aaa66dfa7578f276b193a5bbee5af0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-12-17 00:01:05 +0530

    48: solve

--- problems/48_rotate_image/Solution48.java
+++ problems/48_rotate_image/Solution48.java
@@ -1,5 +1,30 @@
-class Solution48 {
-    public void rotate(int[][] matrix) {
+import java.util.Arrays;
 
+class Solution48 {
+    private void cyclicRotation(int[][] matrix, int r, int k, int n) {
+        int x = matrix[r][r + k];
+        matrix[r][r + k] = matrix[n - r - (k + 1)][r];
+        matrix[n - r - (k + 1)][r] = matrix[n - r - 1][n - r - (k + 1)];
+        matrix[n - r - 1][n - r - (k + 1)] = matrix[r + k][n - r - 1];
+        matrix[r + k][n - r - 1] = x;
+    }
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int r = 0; r <= n / 2; r++) {
+            for (int k = 0; k < n - 2 * r - 1; k++) {
+                cyclicRotation(matrix, r, k, n);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {
+                { 5, 1, 9, 11 },
+                { 2, 4, 8, 10 },
+                { 13, 3, 6, 7 },
+                { 15, 14, 12, 16 } };
+        new Solution48().rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
