a9fd724c12699e6f85d89c63dc3a00c507fc3130
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-07-18 21:41:30 +0530

    49: re-solve

--- problems/49_group_anagrams/Solution49.java
+++ problems/49_group_anagrams/Solution49.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 class Solution49 {
+    // We create it once so that it can be used for every string
+    private int[] counts = new int[26];
+
     // Key of a string is its sorted chars array string
     // which will be same for all the anagrams
     private String getKey2(String str) {
         char[] chars = str.toCharArray();
         Arrays.sort(chars);
-        return Arrays.toString(chars);
+
+        // new String(chars) is much faster than Arrays.toString(chars)
+        return new String(chars);
     }
 
     // Key of a string is the counts array string
     // which will be same for all the anagrams
-    private String getKey1(String str, int[] counts) {
+    private String getKey1(String str) {
         Arrays.fill(counts, 0);
         for (char c : str.toCharArray()) {
             counts[c - 'a']++;
@@ -23,21 +28,19 @@ class Solution49 {
         return Arrays.toString(counts);
     }
 
+    private String getKey(String str) {
+        return getKey1(str);
+    }
+
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
-        int[] counts = new int[26];
         for (String str : strs) {
-            String key = getKey1(str, counts);
-            List<String> anagramGroup = map.getOrDefault(key, new ArrayList<>());
-            anagramGroup.add(str);
-            map.put(key, anagramGroup);
+            String key = getKey(str);
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+            map.get(key).add(str);
         }
 
-        List<List<String>> anagramGroups = new ArrayList<>();
-        for (List<String> anagramGroup : map.values()) {
-            anagramGroups.add(anagramGroup);
-        }
-
-        return anagramGroups;
+        return new ArrayList<>(map.values());
     }
 }
