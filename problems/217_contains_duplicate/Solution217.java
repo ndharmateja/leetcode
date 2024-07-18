381bccdc9d9272a366d4d91055f6a02d3e1934dd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-07-18 20:52:28 +0530

    217: add solution 3

--- problems/217_contains_duplicate/Solution217.java
+++ problems/217_contains_duplicate/Solution217.java
@@ -23,6 +23,17 @@ class Solution217 {
         return false;
     }
 
+    // O(n^2) time and O(1) space
+    private boolean solution3(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+
     public boolean containsDuplicate(int[] nums) {
         return solution1(nums);
     }
