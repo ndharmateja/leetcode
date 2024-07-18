ba266f1d2d01395f9db63b839800d2ef4ac69f12
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-07-18 21:55:30 +0530

    20: re-solve

--- problems/20_valid_paranthesis/Solution20.java
+++ problems/20_valid_paranthesis/Solution20.java
@@ -1,30 +1,39 @@
 import java.util.Stack;
 
 class Solution20 {
-    private boolean isOpening(char c) {
-        return c == '(' || c == '[' || c == '{';
+    private static boolean isOpening(char c) {
+        return c == '(' || c == '{' || c == '[';
     }
 
-    private boolean isMatching(char opening, char closing) {
-        return (opening == '(' && closing == ')') || (opening == '[' && closing == ']')
-                || (opening == '{' && closing == '}');
+    private static boolean isClosing(char c) {
+        return c == ')' || c == '}' || c == ']';
+    }
+
+    private static boolean areMatching(char opening, char closing) {
+        return (opening == '(' && closing == ')')
+                || (opening == '{' && closing == '}')
+                || (opening == '[' && closing == ']');
     }
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+
         for (char c : s.toCharArray()) {
             if (isOpening(c)) {
-                stack.add(c);
-            } else {
-                if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (isClosing(c)) {
+                // If the character doesn't match with the top stack char
+                // or if the stack is empty, it is not valid
+                if (stack.isEmpty() || !areMatching(stack.pop(), c))
                     return false;
-                }
-                char opening = stack.pop();
-                if (!isMatching(opening, c)) {
-                    return false;
-                }
             }
+
+            // We don't need to do anything if char is anything other than
+            // a closing or an opening bracket
         }
+
+        // If the stack is non-empty after going through all the chars
+        // it is not valid
         return stack.isEmpty();
     }
 }
