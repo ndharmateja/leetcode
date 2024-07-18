4287aa463ff06023ca68350044daebfb8815811a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-07-18 21:17:56 +0530

    1: resolve

--- problems/1_two_sum/Solution1.java
+++ problems/1_two_sum/Solution1.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.Map;
 
 @SuppressWarnings("unused")
 class Solution1 {
@@ -16,22 +15,18 @@ class Solution1 {
     }
 
     // O(n) time - using HashMap
-    // Store each value and its index in the hash map as
-    // we iterate
-    // If the complement value exists in the map
-    // we return the indices
+    // Store each value and its index in the hash map as we iterate
+    // If the complement value exists in the map we return the indices
     private int[] solution2(int[] nums, int target) {
-        Map<Integer, Integer> numIndexes = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            int diff = target - num;
-
-            if (numIndexes.containsKey(diff)) {
-                return new int[] { numIndexes.get(diff), i };
-            }
-
-            numIndexes.put(num, i);
+            int complement = target - num;
+            if (map.containsKey(complement))
+                return new int[] { i, map.get(complement) };
+            map.put(num, i);
         }
+
         return null;
     }
 
