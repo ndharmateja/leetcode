a92069426dbae292bd1cb316c2ca4d72e7378047
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-10-16 19:49:05 +0530

    39: add java second solutoin

--- problems/0039-combination-sum/Solution39.java
+++ problems/0039-combination-sum/Solution39.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 class Solution39 {
     private void backtrack(List<List<Integer>> output, List<Integer> currList, int[] nums, int maxIndex, int currSum,
             int target) {
@@ -34,10 +35,52 @@ class Solution39 {
         }
     }
 
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    private List<List<Integer>> solution1(int[] nums, int target) {
         List<List<Integer>> output = new ArrayList<>();
         backtrack(output, new ArrayList<>(), nums, 0, 0, target);
         return output;
+    }
+
+    private void dfs(int[] nums, int target, int i, int currSum, List<Integer> currList, List<List<Integer>> output) {
+        // If the currSum crosses the target, then there is no point continuing
+        // as all the numbers are positive
+        if (currSum > target)
+            return;
+
+        // If targer sum is found, we can add this sequence to our result and exit
+        if (currSum == target) {
+            output.add(new ArrayList<>(currList));
+            return;
+        }
+
+        // If we reach the end, we don't need to do anything
+        // so we can simply return
+        if (i == nums.length)
+            return;
+
+        // Two options pick the current one or don't pick the current one
+        // 1. If we pick the current one, we add that to the current sum
+        // and the current list to track the sequence and then
+        // we call the dfs starting from *the same element* again
+        // as each element can be picked multiple times
+        // That is why we call dfs again from 'i' instead of 'i+1'
+        currList.add(nums[i]);
+        dfs(nums, target, i, currSum + nums[i], currList, output);
+        currList.remove(currList.size() - 1);
+
+        // 2. If we don't pick the current element, we call dfs starting from the
+        // next element
+        dfs(nums, target, i + 1, currSum, currList, output);
+    }
+
+    private List<List<Integer>> solution2(int[] nums, int target) {
+        List<List<Integer>> output = new ArrayList<>();
+        dfs(nums, target, 0, 0, new ArrayList<>(), output);
+        return output;
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        return solution2(nums, target);
     }
 
     public static void main(String[] args) {
