cef9ff51f4f5fe53fc2bc9c627df5ee8b7b165a3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-10-18 13:19:08 +0530

    1143: add 2 solutions in python

--- problems/1143-longest-common-subsequence/Solution1143.py
+++ problems/1143-longest-common-subsequence/Solution1143.py
@@ -1,4 +1,4 @@
-class Solution1143:
+class Solution:
     def lcs_helper(self, text1: str, text2: str, memo: dict) -> int:
         if len(text1) == 0 or len(text2) == 0:
             return 0
@@ -13,5 +13,48 @@ class Solution1143:
                 memo[key] = max(val1, val2, val3)
         return memo[key]
 
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def solution1(self, text1: str, text2: str) -> int:
         return self.lcs_helper(text1, text2, {})
+
+    def solution2(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        memo = {}
+
+        def dfs(i, j):
+            if i == m or j == n:
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if text1[i] == text2[j]:
+                result = 1 + dfs(i + 1, j + 1)
+            else:
+                result = max(dfs(i + 1, j), dfs(i, j + 1))
+            memo[(i, j)] = result
+            return result
+
+        return dfs(0, 0)
+
+    def solution3(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+        dp[m][n] = 0
+        for i in range(m):
+            dp[i][n] = 0
+        for i in range(n):
+            dp[m][i] = 0
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return self.solution3(text1, text2)
