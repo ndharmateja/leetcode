130cf87dedbc43f8d6c86b5c43371944877f28fe
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-02-03 12:12:31 +0530

    21: improve python solution

--- problems/0021-merge-two-sorted-lists/Solution21.py
+++ problems/0021-merge-two-sorted-lists/Solution21.py
@@ -6,44 +6,33 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
+
 class Solution21:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return None
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
+    def mergeTwoLists(
+        self, head1: Optional[ListNode], head2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if head1 is None:
+            return head2
+        if head2 is None:
+            return head1
 
+        curr1, curr2 = head1, head2
+        dummy: ListNode = ListNode()
+        tail: ListNode = dummy
 
-        head = None
-        if list1.val <= list2.val:
-            head = list1
-            list1 = list1.next
-        else:
-            head = list2
-            list2 = list2.next
-
-        curr: ListNode = head
-        while list1 is not None and list2 is not None:
-            if list1.val <= list2.val:
-                curr.next = list1
-                list1 = list1.next
-                curr = curr.next
+        while curr1 and curr2:
+            if curr1.val <= curr2.val:
+                tail.next = curr1
+                curr1 = curr1.next
             else:
-                curr.next = list2
-                list2 = list2.next
-                curr = curr.next
+                tail.next = curr2
+                curr2 = curr2.next
+            tail = tail.next
 
-        while list1 is not None:
-            curr.next = list1
-            list1 = list1.next
-            curr = curr.next
+        if curr1:
+            tail.next = curr1
+        if curr2:
+            tail.next = curr2
 
-        while list2 is not None:
-            curr.next = list2
-            list2 = list2.next
-            curr = curr.next
-
-        return head
+        return dummy.next
