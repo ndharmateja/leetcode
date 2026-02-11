55fcf534e5fe27f738a6682619232e01570b9974
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-02-11 11:50:30 +0530

    19: add a more readable two ptr solution

--- problems/0019-remove-nth-node-from-end-of-list/Solution19.java
+++ problems/0019-remove-nth-node-from-end-of-list/Solution19.java
@@ -44,7 +44,44 @@ class Solution19 {
         return head;
     }
 
-    private ListNode solution2(ListNode head, int n) {
+    // Two ptr solution but with prev and curr
+    private ListNode solution2(ListNode head, int k) {
+        // Move advance 'k' steps forward
+        // so that we can maintain a difference of 'k' nodes
+        // between curr and advance
+        ListNode advance = head;
+        for (int i = 0; i < k; i++)
+            advance = advance.next;
+
+        // At this point if advance is null, it means that the head node
+        // is the one that needs to be deleted as this happens only when k = size
+        if (advance == null)
+            return head.next;
+
+        // Now that the difference between curr and advance is exactly k nodes
+        // we can move them in tandem such that by the time
+        // advance reaches null, curr would exactly reach the
+        // node that we need to delete
+        // So we would keep track of a prev pointer which points to the node
+        // just before the curr node
+        ListNode prev = null, curr = head;
+        while (advance != null) {
+            advance = advance.next;
+            prev = curr;
+            curr = curr.next;
+        }
+
+        // curr points to the node which we need to delete
+        // so we delete it
+        // prev won't be null as we already handled the case where the head needs to be
+        // deleted
+        // and the head would still remain same
+        // because we already handled the case where head needs to be deleted
+        prev.next = curr.next;
+        return head;
+    }
+
+    private ListNode solution3(ListNode head, int n) {
         ListNode curr = head;
         int size = 0;
 
@@ -68,6 +105,6 @@ class Solution19 {
     }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return solution2(head, n);
+        return solution3(head, n);
     }
 }
