63fb5e3f31e3ae65960a5f6629cba2a92cfe35e0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-02 10:36:41 +0530

    104: add recursive solution in c++

---
+++ problems/0104-maximum-depth-of-binary-tree/main.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+private:
+    struct TreeNode
+    {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    int max(int a, int b) { return a >= b ? a : b; }
+
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
