f7a5fd9c61a7a1338001db16c75c688dc1a7fc61
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-02 18:20:36 +0530

    347: add heap based solution in c++

--- problems/0347-top-k-frequent-elements/main.cpp
+++ problems/0347-top-k-frequent-elements/main.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 class Solution
 {
@@ -57,6 +58,55 @@ private:
         return {};
     }
 
+    /**
+     * Min heap solution, keep track of the top-k elements
+     * Theta(n log k) time and Theta(n) space
+     */
+    static std::vector<int> solution2(std::vector<int> &nums, int k)
+    {
+        // Get the counts of each number
+        std::unordered_map<int, int> counts(nums.size());
+        for (const auto &num : nums)
+        {
+            // try_emplace is better than counts[num]++ (which also works)
+            // because it will have to do a double hash lookup during the first insert
+            // try_emplace will insert the key-value pair only if the key doesn't
+            // already exist and if key already exists, it doesn't do anything
+            auto [it, inserted] = counts.try_emplace(num, 0);
+            it->second++;
+        }
+
+        // Create a min heap that holds only k elements at any point
+        // std::greater<> works because the pair class has comparison built into it
+        // but the comparison happens based on the first values, so we need to
+        // put the count as the first value of the pair
+        // std::greater<> should be used for min heap
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> min_heap;
+        for (const auto &[num, count] : counts)
+        {
+            // Insert the pair of {count, num} in the min heap
+            // count should be the first value as we are comparing pairs directly
+            // and the comparison is based on the first values
+            min_heap.push({count, num});
+
+            // If the number of elements in heap cross k, we remove the min
+            if (min_heap.size() > k)
+                min_heap.pop();
+        }
+
+        // At this point we have the top k elements in the min heap
+        // We can collect all the k numbers (the second values of the pair objects)
+        // Better to create a k sized vector directly and then assign elements
+        // instead of creating an empty list and then pushing elements due to resizing
+        std::vector<int> result(k);
+        for (size_t i = 0; i < k; i++)
+        {
+            result[i] = min_heap.top().second;
+            min_heap.pop();
+        }
+        return result;
+    }
+
 public:
-    std::vector<int> topKFrequent(std::vector<int> &nums, int k) { return solution1(nums, k); }
+    std::vector<int> topKFrequent(std::vector<int> &nums, int k) { return solution2(nums, k); }
 };
