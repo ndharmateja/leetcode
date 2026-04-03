5a6c5621d29a5275b59ef2da90faf8d8d11caf70
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-03 20:06:36 +0530

    703: add comments about a tricky case

--- problems/0703-kth-largest-element-in-a-stream/main.cpp
+++ problems/0703-kth-largest-element-in-a-stream/main.cpp
@@ -12,16 +12,24 @@ private:
 public:
     KthLargest(int k, std::vector<int> &nums) : k{k}
     {
+        // Add all the nums to the pq, so that the size of the pq
+        // never crosses 'k'
+        // To save the overhead 
         for (const auto &num : nums)
             this->add(num);
     }
 
     int add(int val)
     {
+        // Since k <= nums.length + 1 and nums.length can be 0 and k be 1
+        // after the constructor, pq can be empty
+        // So instead of optimizing it to add only when the val > currmin in heap
+        // we unconditionally add the value and pop if size crosses k to maintain
+        // the heap at k elements always
         this->pq.push(val);
 
-        // If there are more than k elements, we remove the min
-        if (pq.size() > k)
+        // If the size crosses k, remove the min
+        if (this->pq.size() > k)
             this->pq.pop();
 
         // Return the kth largest element which is the min in the heap (if
