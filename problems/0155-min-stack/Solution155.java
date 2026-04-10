6a91e6d13bf73e1e836ca654fb266cad1b39ef22
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-10 09:50:38 +0530

    155: add packed long solution in java

--- problems/0155-min-stack/Solution155.java
+++ problems/0155-min-stack/Solution155.java
@@ -1,40 +1,71 @@
 import java.util.Stack;
 
+@SuppressWarnings("unused")
 class Solution155 {
+    private class MinStack1 {
+        private Stack<Long> packedStack;
 
-    private Stack<Integer> stack;
-    private Stack<Integer> minValues;
+        public MinStack1() {
+            this.packedStack = new Stack<>();
+        }
 
-    public Solution155() {
-        stack = new Stack<>();
-        minValues = new Stack<>();
+        public void push(int val) {
+            int min = this.packedStack.isEmpty() || val < this.getMin() ? val : this.getMin();
+            this.packedStack.add(pack(min, val));
+        }
+
+        public void pop() {
+            this.packedStack.pop();
+        }
+
+        public int top() {
+            return (int) (this.packedStack.peek() & 0xFFFFFFFFL);
+        }
+
+        public int getMin() {
+            return (int) (this.packedStack.peek() >> 32);
+        }
+
+        private long pack(int min, int val) {
+            return ((long) min << 32) | (val & 0xFFFFFFFFL);
+        }
     }
 
-    public void push(int val) {
-        if (stack.isEmpty()) {
+    private class MinStack2 {
+        private Stack<Integer> stack;
+        private Stack<Integer> minValues;
+
+        public MinStack2() {
+            stack = new Stack<>();
+            minValues = new Stack<>();
+        }
+
+        public void push(int val) {
+            if (stack.isEmpty()) {
+                stack.push(val);
+                minValues.push(val);
+                return;
+            }
             stack.push(val);
-            minValues.push(val);
-            return;
+            if (minValues.peek() > val) {
+                minValues.push(val);
+            } else {
+                minValues.push(minValues.peek());
+            }
         }
-        stack.push(val);
-        if (minValues.peek() > val) {
-            minValues.push(val);
-        } else {
-            minValues.push(minValues.peek());
+
+        public void pop() {
+            stack.pop();
+            minValues.pop();
         }
-    }
 
-    public void pop() {
-        stack.pop();
-        minValues.pop();
-    }
+        public int top() {
+            return stack.peek();
+        }
 
-    public int top() {
-        return stack.peek();
-    }
-
-    public int getMin() {
-        return minValues.peek();
+        public int getMin() {
+            return minValues.peek();
+        }
     }
 }
 
