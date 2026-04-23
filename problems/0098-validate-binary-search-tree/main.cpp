e05c1bd0e7de64f1fd226b8e4d8f22692d5e0dfb
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-23 19:55:32 +0530

    98: add solution 2 in c++

--- problems/0098-validate-binary-search-tree/main.cpp
+++ problems/0098-validate-binary-search-tree/main.cpp
@@ -35,6 +35,46 @@ private:
         return solution1(root, min_int, max_int);
     }
 
+    static bool solution2(TreeNode *root, int min, int max)
+    {
+        // If no node, it is trivially true
+        if (!root)
+            return true;
+
+        // First check if the root's value is in [min, max]
+        if (root->val < min || root->val > max)
+            return false;
+
+        // At this point, root's value is in [min, max]
+        // If the root's value is already min and has a left child we can
+        // directly return false as we know that all the values in the tree
+        // are unique
+        // Similarly for the right side
+        if (root->val == min && root->left)
+            return false;
+        if (root->val == max && root->right)
+            return false;
+
+        // Check if the left subtree and the right subtree are in the valid range
+        // only if they exist
+        // We won't get an overflow issue as we already considered the case
+        // where the left child exists and the value of the root itself is min
+        // and also we are checking if the root's left exists
+        if (root->left && !solution2(root->left, min, root->val - 1))
+            return false;
+        if (root->right && !solution2(root->right, root->val + 1, max))
+            return false;
+
+        // Return the accumulated result
+        return true;
+    }
+
+    static bool solution2(TreeNode *root)
+    {
+        int min_int = std::numeric_limits<int>::min();
+        int max_int = std::numeric_limits<int>::max();
+        return solution2(root, min_int, max_int);
+    }
 public:
     bool isValidBST(TreeNode *root) { return solution1(root); }
 };
