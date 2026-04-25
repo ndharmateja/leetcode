4b59364ab2fd56de3c452a1706103be4aad3d41d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-25 11:18:25 +0530

    230: move TreeNode struct outside

--- problems/0230-kth-smallest-element-in-a-bst/main.cpp
+++ problems/0230-kth-smallest-element-in-a-bst/main.cpp
@@ -1,19 +1,19 @@
 #include <stack>
 #include <vector>
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
 private:
-    struct TreeNode
-    {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
-        TreeNode() : val(0), left(nullptr), right(nullptr) {}
-        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    };
-
     static void inorder_traversal(TreeNode *root, std::vector<int> &result)
     {
         if (!root)
