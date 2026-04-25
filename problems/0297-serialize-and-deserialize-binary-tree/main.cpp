42d70d79d49070fe3e59e769ae362af56e05b0cc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-25 13:15:39 +0530

    297: add Theta(n) solution to Solution2 namespace

--- problems/0297-serialize-and-deserialize-binary-tree/main.cpp
+++ problems/0297-serialize-and-deserialize-binary-tree/main.cpp
@@ -9,6 +9,177 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
+namespace Solution2
+{
+    class Codec
+    {
+    private:
+        /**
+         * Recursively serializes the tree rooted at root according to preorder
+         * traversal. We use empty brackets for empty children as otherwise we
+         * wouldn't know if it is the left/right child when there is only one child
+         *
+         * Eg: If the tree is
+         *       A
+         *      / \
+         *     B   C
+         *    / \
+         *   D   E
+         * Then the serialized string would be: A(B(D()())(E()()))(C())
+         */
+        static void serialize(TreeNode *root, std::string &result)
+        {
+            // If the root is nullptr, then nothing to add to the string
+            if (!root)
+                return;
+
+            // Since it is preorder, we first append the value of the root
+            result.append(std::to_string(root->val));
+
+            // We append the serialized string (which is recursively computed)
+            // of the left subtree (could be potentially nullptr in which case empty string)
+            // between paranthesis
+            result.push_back('(');
+            serialize(root->left, result);
+            result.push_back(')');
+
+            // We do the same for the right subtree
+            result.push_back('(');
+            serialize(root->right, result);
+            result.push_back(')');
+        }
+
+        /**
+         * Parses the integer in the data string starting at index i
+         * Makes i the index after the number part ends
+         * Also parses negative values
+         *
+         * Eg:
+         *     i
+         *     ↓
+         *  ...-125...
+         * parses and returns -125 as the answer and makes i point to the index after -125
+         *        i
+         *        ↓
+         * ...-125...
+         */
+        static int parse_integer(const std::string &data, int &i)
+        {
+            // Error checking. The character at should either be an int or a '-' char
+            unsigned char c = data[i];
+            if (!std::isdigit(c) && c != '-')
+                throw std::runtime_error("Invalid input");
+
+            // Handle negative values
+            bool is_negative = false;
+            if (c == '-')
+            {
+                is_negative = true;
+                i++;
+            }
+
+            // Parse the number using Horner's method
+            int result = 0;
+            while (std::isdigit(c = data[i]))
+            {
+                result = (result * 10) + (c - '0');
+                i++;
+            }
+
+            // At this point, i will point to the first character after the number ends
+            // If is_negative is true, we flip the sign of the result
+            return is_negative ? -result : result;
+        }
+
+        /**
+         * Recursive function that deserializes a tree starting at index i
+         * Makes i point to the after the subtree rooted at the value starting at index 'i'
+         *
+         *       2              17
+         *       ↓              ↓
+         * Eg: A(B(D()())(E()()))(C())
+         * is the tree
+         *       A
+         *      / \
+         *     B   C
+         *    / \
+         *   D   E
+         *
+         * If we call deserialize(data, 2)
+         * we need to parse the tree rooted at B
+         * and this function makes i point to 17 which is the closing parenthesis
+         * after the tree string of B ends
+         */
+        static TreeNode *deserialize(const std::string &data, int &i)
+        {
+            // If the char at 'i' is ')' it means that there is the empty paranthesis of the empty node
+            // so we can return nullptr
+            unsigned char c = data[i];
+            if (c == ')')
+                return nullptr;
+
+            // Parse the integer starting at index 'start'
+            int root_val = parse_integer(data, i);
+
+            // Create the root node
+            TreeNode *root = new TreeNode(root_val);
+
+            // At this point i points to the closing paranthesis outside the left subtree
+            //     i
+            //     ↓
+            // root(left-subtree)(right-subtree)
+            // So we increment 'i' to be able to pass it to the recursive call to parse
+            // the left subtree
+            i++;
+
+            // Parse the left subtree
+            root->left = deserialize(data, i);
+
+            // At this point i points to the closing paranthesis outside the left subtree
+            //                  i
+            //                  ↓
+            // root(left-subtree)(right-subtree)
+            // so we increment it twice to point to the start of the subtree string of the right subtree
+            i += 2;
+
+            // Parse the right subtree
+            root->right = deserialize(data, i);
+
+            // At this point i points to the closing paranthesis outside the right subtree
+            //                                 i
+            //                                 ↓
+            // root(left-subtree)(right-subtree)
+            // but according to the recursive function contract, i should point to the index
+            // after the tree rooted at root ends. so we increment 'i'
+            i++;
+
+            // Return the parsed tree
+            return root;
+        }
+
+    public:
+        // Encodes a tree to a single string.
+        std::string serialize(TreeNode *root)
+        {
+            std::string result;
+            serialize(root, result);
+            return result;
+        }
+
+        // Decodes your encoded data to tree.
+        TreeNode *deserialize(const std::string &data)
+        {
+            // Empty string case
+            if (data.empty())
+                return nullptr;
+
+            // Parse the non-empty tree
+            int i = 0;
+            return deserialize(data, i);
+        }
+    };
+}
+
 namespace Solution1
 {
     class Codec
