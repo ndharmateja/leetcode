6a40df580410e19e2b6cbe50cbf7af242340aaff
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-29 17:00:31 +0530

    97: fix bug

--- problems/0097-interleaving-string/main.cpp
+++ problems/0097-interleaving-string/main.cpp
@@ -57,7 +57,7 @@ private:
         for (int i = 1; i <= m; i++)
             dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
         for (int j = 1; j <= n; j++)
-            dp[0][j] = dp[0][j - 1] && s1[j - 1] == s3[j - 1];
+            dp[0][j] = dp[0][j - 1] && s2[j - 1] == s3[j - 1];
 
         // Fill the table
         for (int i = 1; i <= m; i++)
