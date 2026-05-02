0ab4fce155eb71b2f6f2266c9ac29a5c98f27e46
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-02 12:08:00 +0530

    332: add skeleton code

---
+++ problems/0332-reconstruct-itinerary/main.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+
+class Solution
+{
+    // Ideas:
+    // 1. Euler's walk
+    // 2. MIT 6.006 lecture 19 (replicating graph to represent time) and then find the topological order
+    // by converting topological sort in a graph to a topological sort in dAG
+public:
+    std::vector<std::string> findItinerary(const std::vector<std::vector<std::string>> &tickets) {}
+};
