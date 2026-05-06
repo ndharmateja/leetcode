bc690bf6d76c7bb9bb45fabfb6320e1165262c75
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-06 16:08:39 +0530

    127: add bidirectional bfs solution in c++

--- problems/0127-word-ladder/main.cpp
+++ problems/0127-word-ladder/main.cpp
@@ -136,8 +136,6 @@ private:
                 // Pop the next element and if it matches
                 std::string curr = queue.front();
                 queue.pop();
-                if (curr == end_word)
-                    return distance;
 
                 // Add all the unvisited neighbours of the word to the queue
                 buffer = curr;
@@ -151,6 +149,8 @@ private:
                     {
                         if (neighbour == curr)
                             continue;
+                        if (neighbour == end_word)
+                            return distance + 1;
                         if (visited.count(neighbour))
                             continue;
                         queue.push(neighbour);
@@ -262,8 +262,6 @@ private:
             {
                 // Pop the next element and if it matches
                 int curr_index = queue.front();
-                if (curr_index == end_word_index)
-                    return distance;
                 queue.pop();
 
                 // Add all the unvisited neighbours of the word to the queue
@@ -289,6 +287,10 @@ private:
                             if (neighbour_index == curr_index)
                                 continue;
 
+                            // If this is the answer we can return
+                            if (neighbour_index == end_word_index)
+                                return distance + 1;
+
                             // If the neighbour is already visited, we don't have to process it
                             if (visited[neighbour_index])
                                 continue;
@@ -313,13 +315,217 @@ private:
     }
 
     /**
-     * TODO: Bidirectional BFS
+     * Bidirectional BFS
+     * TODO: modularity, add running time and space, add detailed explanation and argument for correctness
      */
     static int solution4(const std::string &begin_word, const std::string &end_word, const std::vector<std::string> &word_list)
     {
-        return -1;
+        /**
+         * Create the adjacency list of patterns
+         *                         0      1      2      3      4      5
+         * Say the word list is ["hot", "dot", "dog", "lot", "log", "cog"]
+         * Then the pattern adjacency list will be
+         * {
+         *    "*ot": [0, 1, 3],
+         *    "h*t": [0],
+         *    "ho*": [0],
+         *    "d*t": [1],
+         *    "do*": [1, 2],
+         *    "*og": [2, 4, 5],
+         *    "d*g": [2],
+         *    "l*t": [3],
+         *    "lo*": [3, 4],
+         *    "l*g": [4],
+         *    "c*g": [5],
+         *    "co*": [5]
+         * }
+         */
+
+        // m keeps track of the length of the word, n keeps track of #words in the word list
+        // end_word_index and begin_word_index keep track of the indices of end_word and begin_word
+        // We are using a default value of 'n' for the begin_word as it need not occur in the word list
+        // and we still need to make it work for the visited boolean vector
+        // So if the begin word doesn't occur in the word list, we use an index of n for it
+        int m{static_cast<int>(begin_word.size())}, n{static_cast<int>(word_list.size())};
+        int end_word_index{-1}, begin_word_index{n};
+
+        // The buffer string is used to create different patterns from the word
+        // for the adjacency list and also for the neighbours in the BFS main loop
+        std::string buffer(m, '\0');
+
+        // Create the pattern adjacency list
+        // For each word, we create all patterns and add the word index to all the pattern keys
+        std::unordered_map<std::string, std::vector<int>> pattern_adj_list;
+        for (int i = 0; i < n; i++)
+        {
+            // Check if end word and begin word occur in the word list
+            buffer = word_list[i];
+            if (buffer == end_word)
+                end_word_index = i;
+            if (buffer == begin_word)
+                begin_word_index = i;
+
+            // Use the buffer string as a buffer to create different patterns from the word
+            // Eg: if word is "hot", then buffer takes the following values in each of the iterations
+            // ["*ot", "h*t", "ho*"]
+            for (int j = 0; j < m; j++)
+            {
+                // Create the pattern with the ith char being '*'
+                unsigned char c_j = buffer[j];
+                buffer[j] = '*';
+
+                // Add the word to the adj list
+                pattern_adj_list[buffer].push_back(i);
+
+                // Reset the word for the next iteration
+                buffer[j] = c_j;
+            }
+        }
+
+        // Edge case, if endword is not in the wordlist, we can return 0
+        if (end_word_index == -1)
+            return 0;
+
+        // Start BFS from both the begin word and the end word
+        // Create the queue and visited for BFS
+        // The visited vector need not be of size n+1 even though the begin word if
+        // not present in the word list has an index of n+1 as, if the begin word is not
+        // in the word list, it would not appear in any of the adjacency list keys
+        // So we need not keep track of the begin word in the visited vector
+        // If the begin word occurred in the word list, then it will be kept track of
+        // by its index
+
+        // Data structures for BFS from the begin word
+        std::vector<bool> visited_front(n, false);
+        std::queue<int> queue_front;
+        queue_front.push(begin_word_index);
+        int distance_from_front{1};
+
+        // Data structures for BFS from the end word
+        std::vector<bool> visited_back(n, false);
+        std::queue<int> queue_back;
+        queue_back.push(end_word_index);
+        int distance_from_back{1};
+
+        while (!queue_front.empty() && !queue_back.empty())
+        {
+            // Run a layer of BFS from the front side
+            int num_elements_layer{static_cast<int>(queue_front.size())};
+            for (int i = 0; i < num_elements_layer; i++)
+            {
+                // Pop the next element and if it matches
+                int curr_index = queue_front.front();
+                queue_front.pop();
+
+                // Add all the unvisited neighbours of the word to the queue
+                buffer = curr_index == n ? begin_word : word_list[curr_index];
+                for (int j = 0; j < m; j++)
+                {
+                    // Create the pattern with the jth char being '*'
+                    // Say the word is "hot" and j is 1, then pattern is "h*t"
+                    // c_j = 'o' to reset the jth char for the next iteration
+                    unsigned char c_j = buffer[j];
+                    buffer[j] = '*';
+
+                    // Get all the neighbours with the matching pattern except for curr itself
+                    // only if the pattern exists in the adjacency list
+                    auto it = pattern_adj_list.find(buffer);
+                    if (it != pattern_adj_list.end())
+                        for (int neighbour_index : it->second)
+                        {
+                            // If the neighbour is the curr word itself (it is possible that they are
+                            // the same as we are finding the neighbours based on the pattern)
+                            // Eg: if curr is "hot" and pattern is "*ot", then "hot" also will be part of the
+                            // "*ot" pattern in the adjacency list
+                            if (neighbour_index == curr_index)
+                                continue;
+
+                            // If we directly encounter the end word we can return
+                            if (neighbour_index == end_word_index)
+                                return distance_from_front + 1;
+
+                            // If the neighbour is already visited, we don't have to process it
+                            if (visited_front[neighbour_index])
+                                continue;
+
+                            // If this word was already visited by the BFS from the back
+                            // We found the shortest path, length = (distance_from_front + 1) + (distance_from_back) - 1;
+                            if (visited_back[neighbour_index])
+                                return distance_from_front + distance_from_back;
+
+                            // !We mark the neighbour visited here to prevent duplicate insertions into the queue
+                            // Mark the neighbour as visited and push it to the queue
+                            visited_front[neighbour_index] = true;
+                            queue_front.push(neighbour_index);
+                        }
+
+                    // Reset the word for the next iteration
+                    buffer[j] = c_j;
+                }
+            }
+
+            // As one layer is completed, we increment the distance for the next layer
+            distance_from_front++;
+
+            // Run a layer of BFS from the back side
+            num_elements_layer = static_cast<int>(queue_back.size());
+            for (int i = 0; i < num_elements_layer; i++)
+            {
+                // Pop the next element and if it matches
+                int curr_index = queue_back.front();
+                queue_back.pop();
+
+                // Add all the unvisited neighbours of the word to the queue
+                buffer = curr_index == n ? begin_word : word_list[curr_index];
+                for (int j = 0; j < m; j++)
+                {
+                    // Create the pattern with the jth char being '*'
+                    // Say the word is "hot" and j is 1, then pattern is "h*t"
+                    // c_j = 'o' to reset the jth char for the next iteration
+                    unsigned char c_j = buffer[j];
+                    buffer[j] = '*';
+
+                    // Get all the neighbours with the matching pattern except for curr itself
+                    // only if the pattern exists in the adjacency list
+                    auto it = pattern_adj_list.find(buffer);
+                    if (it != pattern_adj_list.end())
+                        for (int neighbour_index : it->second)
+                        {
+                            // If the neighbour is the curr word itself (it is possible that they are
+                            // the same as we are finding the neighbours based on the pattern)
+                            // Eg: if curr is "hot" and pattern is "*ot", then "hot" also will be part of the
+                            // "*ot" pattern in the adjacency list
+                            if (neighbour_index == curr_index)
+                                continue;
+
+                            // If the neighbour is already visited, we don't have to process it
+                            if (visited_back[neighbour_index])
+                                continue;
+
+                            // If this word was already visited by the BFS from the back
+                            // We found the shortest path
+                            if (visited_front[neighbour_index])
+                                return distance_from_front + distance_from_back;
+
+                            // !We mark the neighbour visited here to prevent duplicate insertions into the queue
+                            // Mark the neighbour as visited and push it to the queue
+                            visited_back[neighbour_index] = true;
+                            queue_back.push(neighbour_index);
+                        }
+
+                    // Reset the word for the next iteration
+                    buffer[j] = c_j;
+                }
+            }
+
+            // As one layer is completed, we increment the distance for the next layer
+            distance_from_back++;
+        }
+
+        // We would not reach here
+        return 0;
     }
 
 public:
-    int ladderLength(const std::string &begin_word, const std::string &end_word, const std::vector<std::string> &word_list) { return solution3(begin_word, end_word, word_list); }
+    int ladderLength(const std::string &begin_word, const std::string &end_word, const std::vector<std::string> &word_list) { return solution4(begin_word, end_word, word_list); }
 };
