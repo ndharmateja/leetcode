6f35508cf925ffc9522a7609554fb13477ec1540
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-11 20:27:41 +0530

    494: add vector as memo solution

--- problems/0494-target-sum/main.cpp
+++ problems/0494-target-sum/main.cpp
@@ -60,6 +60,67 @@ private:
         return sol1(nums, n - 1, sum, memo);
     }
 
+    static int sol2(const std::vector<int> &nums,
+                    int i, int target, int total_sum,
+                    std::vector<std::vector<int>> &memo)
+    {
+        // Base case
+        // ! We are not storing the base cases in the memo to store space as there
+        // ! could be multiple base cases
+        // We can do that by returning a sum of values
+        // We need to add both cases as sum and nums.front() can both be 0
+        // in which case the #ways would be 2
+        if (i == 0)
+            return (target == nums.front() ? 1 : 0) + (target == -nums.front() ? 1 : 0);
+
+        // Edge case to prevent index out of bounds for the second index
+        // Eg: nums=[1, 1, 1, 1, 1] and target=-5
+        // In the first recursive call we would check for i = 3 and target = -6 as well
+        // which can make target + total_sum = -1 thereby giving an invalid index
+        if (target < -total_sum || target > total_sum)
+            return 0;
+
+        // If the memo contains the answer already, we can return it
+        // We are forming a 64bit uint64_t key by packing the i and sum next to each other
+        int &result = memo[i][target + total_sum];
+        if (result != -1)
+            return result;
+
+        // Otherwise we insert after recursively computing the solution and return it
+        result = sol2(nums, i - 1, target + nums[i], total_sum, memo) +
+                 sol2(nums, i - 1, target - nums[i], total_sum, memo);
+        return result;
+    }
+
+    /**
+     * Same as solution 1 except we are using a 2D vector of ints as the memo
+     * instead of the unordered map
+     * since target can be -ve, we add it with offset of total_sum to make it >= 0
+     * so that we can use that as the second index
+     */
+    static int sol2(const std::vector<int> &nums, int target)
+    {
+        // Base case
+        // We can do that by returning a sum of values
+        // We need to add both cases as sum and nums.front() can both be 0
+        // in which case the #ways would be 2
+        int n{static_cast<int>(nums.size())};
+        if (n == 1)
+            return (target == nums.front() ? 1 : 0) + (target == -nums.front() ? 1 : 0);
+
+        // Edge case
+        // If target is greater than total_sum or smaller than -total_sum, we can return 0
+        int total_sum{0};
+        for (int num : nums)
+            total_sum += num;
+        if (target < -total_sum || target > total_sum)
+            return 0;
+
+        // General case
+        std::vector<std::vector<int>> memo(n, std::vector<int>(2 * total_sum + 1, -1));
+        return sol2(nums, n - 1, target, total_sum, memo);
+    }
+
 public:
-    int findTargetSumWays(const std::vector<int> &nums, int target) { return sol1(nums, target); }
+    int findTargetSumWays(const std::vector<int> &nums, int target) { return sol2(nums, target); }
 };
