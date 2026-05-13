9482e7c0b4144399ec7ea24a19916091b7a67e83
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-13 09:20:43 +0530

    91: rearrange solutions

--- problems/0091-decode-ways/main.cpp
+++ problems/0091-decode-ways/main.cpp
@@ -5,57 +5,11 @@ class Solution
 {
 private:
     /**
-     * Recursive function (memoized) that computes the #ways we can decode s[i:]
-     */
-    static int sol1(const std::string &s, int i, int n, std::vector<int> &memo)
-    {
-        // Edge case
-        // If string starts with 0, then 0 ways
-        if (s[i] == '0')
-            return 0;
-
-        // Base case
-        // If index out of bounds, we can return 1
-        if (i == n)
-            return 1;
-
-        // If memo contains the answer we can directly return it
-        int &answer = memo[i];
-        if (answer != -1)
-            return answer;
-
-        // We can try to form a number with just s[i] or s[i:i+2]
-        // At this point we know that s[i] is non zero
-        // So we can form a number with s[i]
-        answer = sol1(s, i + 1, n, memo);
-
-        // We can form a number with s[i:i+2] if there are two digits and
-        // it forms a number between 10 and 26
-        if (i <= n - 2 &&
-            ((s[i] == '1') ||
-             (s[i] == '2' && '0' <= s[i + 1] && s[i + 1] <= '6')))
-            answer += sol1(s, i + 2, n, memo);
-
-        // Return the answer
-        return answer;
-    }
-
-    /**
-     * Top down DP approach
-     */
-    static int sol1(const std::string &s)
-    {
-        int n{static_cast<int>(s.size())};
-        std::vector<int> memo(n, -1);
-        return sol1(s, 0, n, memo);
-    }
-
-    /**
      * Bottom up DP approach
      *
      * Theta(n) time and Theta(n) space solution
      */
-    static int sol2(const std::string &s)
+    static int sol1(const std::string &s)
     {
         /**
          * * DP solution
@@ -121,7 +75,53 @@ private:
     /**
      * TODO: Space optimized bottom up DP approach
      */
-    static int sol3(const std::string &s) { return -1; }
+    static int sol2(const std::string &s) { return -1; }
+
+    /**
+     * Recursive function (memoized) that computes the #ways we can decode s[i:]
+     */
+    static int sol3(const std::string &s, int i, int n, std::vector<int> &memo)
+    {
+        // Edge case
+        // If string starts with 0, then 0 ways
+        if (s[i] == '0')
+            return 0;
+
+        // Base case
+        // If index out of bounds, we can return 1
+        if (i == n)
+            return 1;
+
+        // If memo contains the answer we can directly return it
+        int &answer = memo[i];
+        if (answer != -1)
+            return answer;
+
+        // We can try to form a number with just s[i] or s[i:i+2]
+        // At this point we know that s[i] is non zero
+        // So we can form a number with s[i]
+        answer = sol3(s, i + 1, n, memo);
+
+        // We can form a number with s[i:i+2] if there are two digits and
+        // it forms a number between 10 and 26
+        if (i <= n - 2 &&
+            ((s[i] == '1') ||
+             (s[i] == '2' && '0' <= s[i + 1] && s[i + 1] <= '6')))
+            answer += sol3(s, i + 2, n, memo);
+
+        // Return the answer
+        return answer;
+    }
+
+    /**
+     * Top down DP approach
+     */
+    static int sol3(const std::string &s)
+    {
+        int n{static_cast<int>(s.size())};
+        std::vector<int> memo(n, -1);
+        return sol3(s, 0, n, memo);
+    }
 
 public:
     int numDecodings(const std::string &s) { return sol2(s); }
