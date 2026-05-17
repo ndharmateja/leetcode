43ca497ed6ee01c2c24c48e95abdb52b47f1742b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-17 21:42:42 +0530

    787: add bottom up dp solution in c++

---
+++ problems/0787-cheapest-flights-within-k-stops/main.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <utility>
+#include <limits>
+#include <algorithm>
+
+class Solution
+{
+private:
+    static inline const int POS_INF = std::numeric_limits<int>::max();
+
+    static int sum(int x, int y)
+    {
+        if (x == POS_INF || y == POS_INF)
+            return POS_INF;
+        return x + y;
+    }
+
+    static int sol1(int n, const std::vector<std::vector<int>> &flights,
+                    int src, int dst, int k)
+    {
+        k++;
+
+        std::vector<std::vector<std::pair<int, int>>> reverse_adj_list(n);
+        for (const std::vector<int> &flight : flights)
+        {
+            int from = flight[0];
+            int to = flight[1];
+            int cost = flight[2];
+            reverse_adj_list[to].push_back({from, cost});
+        }
+
+        std::vector<std::vector<int>> dp(n, std::vector<int>(k + 1));
+        for (int v = 0; v < n; v++)
+            dp[v][0] = POS_INF;
+        dp[src][0] = 0;
+
+        for (int i = 1; i <= k; i++)
+        {
+            for (int v = 0; v < n; v++)
+            {
+                if (v == src)
+                    continue;
+                int min_value = dp[v][i - 1];
+                for (const auto &[u, cost] : reverse_adj_list[v])
+                    min_value = std::min(min_value, sum(dp[u][i - 1], cost));
+                dp[v][i] = min_value;
+            }
+        }
+
+        return dp[dst][k] == POS_INF ? -1 : dp[dst][k];
+    }
+
+    // static int sol2(std::vector<std::vector<std::pair<int, int>>> reverse_adj_list,
+    //                 int v, int i, int src)
+    // {
+    //     if (v == src)
+    //         return 0;
+    //     if (i == 0)
+    //         return POS_INF;
+
+    // }
+
+    // static int sol2(int n, const std::vector<std::vector<int>> &flights,
+    //                 int src, int dst, int k)
+    // {
+    // }
+
+public:
+    int findCheapestPrice(int n, const std::vector<std::vector<int>> &flights,
+                          int src, int dst, int k) { return sol1(n, flights, src, dst, k); }
+};
