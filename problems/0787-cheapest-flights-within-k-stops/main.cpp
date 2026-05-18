ce05d7e04fdab29db6a7a8866e50cb74e7739a37
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-18 12:11:55 +0530

    787: flip the dimensions of the dp matrix for cache locality

--- problems/0787-cheapest-flights-within-k-stops/main.cpp
+++ problems/0787-cheapest-flights-within-k-stops/main.cpp
@@ -43,23 +43,28 @@ private:
                     int src, int dst, int k)
     {
         /**
+         * ! Was originally dp[v][i] for the length of the shortest path from src to v
+         * ! with atmost 'i' edges
+         * ! We changed it to dp[i][v] for cache locality as we were filling it column
+         * ! by column originally and now we can fill it row by row accessing the values
+         * ! in the previous row increasing cache locality
          * * DP solution:
-         * dp[v][i]: The length of the shortest path from the src to v
-         *           with *atmost* k edges
+         * dp[i][v]: The length of the shortest path from the src to v
+         *           with *atmost* i edges
          * * Base case:
-         * dp[src][i] = 0        for all i in [0, k]
-         * dp[v][0]   = pos_inf  for all v != src
+         * dp[i][src] = 0        for all i in [0, k]
+         * dp[0][v]   = pos_inf  for all v != src
          * As length of the shortest path from src to itself with atmost k edges (for any k)
          * and length of the shortest path from src to any other vertex with atmost 0 edges
          * is infinity as we can't reach any other node from source with atmost 0 edges
          * * Recurrence relation:
-         * dp[v][i] = min{ dp[v][i-1],
-         *                 dp[u][i-1] + l(u, v) for all u->v }
+         * dp[i][v] = min{ dp[i-1][v],
+         *                 dp[i-1][u] + l(u, v) for all u->v }
          * * Order of filling:
-         * Fill each column in any order as dp values in a column depend only upon the
-         * values of the previous column
+         * Fill each row in any order as dp values in a row depend only upon the
+         * values of the previous row
          * * Final answer:
-         * dp[dst][k]
+         * dp[k][dst]
          * * Running time:
          * #subproblems:        n(k + 1)
          * work per subproblem: Theta(indegree)
@@ -82,29 +87,29 @@ private:
         build_reverse_adj_list(flights, reverse_adj_list);
 
         // Init the dp table with 0s and fill the base cases
-        std::vector<std::vector<int>> dp(n, std::vector<int>(k + 1));
+        std::vector<std::vector<int>> dp(k + 1, std::vector<int>(n));
         for (int v = 0; v < n; v++)
-            dp[v][0] = POS_INF;
-        dp[src][0] = 0;
+            dp[0][v] = POS_INF;
+        dp[0][src] = 0;
 
-        // Fill the table column by column from left to right
-        // except for the last column
+        // Fill the table row by row from left to right
+        // except for the last row
         for (int i = 1; i < k; i++)
             for (int v = 0; v < n; v++)
             {
                 if (v == src)
                     continue;
-                int &min_value = dp[v][i];
-                min_value = dp[v][i - 1];
+                int &min_value = dp[i][v];
+                min_value = dp[i - 1][v];
                 for (const auto &[u, cost] : reverse_adj_list[v])
-                    min_value = std::min(min_value, sum(dp[u][i - 1], cost));
+                    min_value = std::min(min_value, sum(dp[i - 1][u], cost));
             }
 
-        // Optimization: we don't need to fill the entire last column
-        // as we only need the dp[dst][k] from the last column
-        int min_value = dp[dst][k - 1];
+        // Optimization: we don't need to fill the entire last row
+        // as we only need the dp[dst][k] from the last row
+        int min_value = dp[k - 1][dst];
         for (const auto &[u, cost] : reverse_adj_list[dst])
-            min_value = std::min(min_value, sum(dp[u][k - 1], cost));
+            min_value = std::min(min_value, sum(dp[k - 1][u], cost));
 
         // Return the answer
         return min_value == POS_INF
@@ -125,14 +130,13 @@ private:
         std::vector<std::vector<std::pair<int, int>>> reverse_adj_list(n);
         build_reverse_adj_list(flights, reverse_adj_list);
 
-        // We only need the values of the previous column
-        // But we cannot have just one column and update itself (like edit distance etc)
+        // We only need the values of the previous row
+        // But we cannot have just one row and update itself (like edit distance etc)
         // as dp[v][i] might need dp[u][i-1] (which would have been updated with dp[u][i] if u < v)
-        // We can have a 1d vector storing one column and a temporary 1d column to store the
-        // results of computation before we completely get rid of the previous column
-        // ! Optimization: To prevent copying to temp vector, we can use 2 columns and use
-        // ! one column for dp values and the other for temp alternating
-        // ! We are storing the 2 columns as 2 rows for cache locality
+        // We can have a 1d vector storing one row and a temporary 1d row to store the
+        // results of computation before we completely get rid of the previous row
+        // ! Optimization: To prevent copying to temp vector, we can use 2 rows and use
+        // ! one row for dp values and the other for temp alternating
         std::vector<std::vector<int>> dp(2, std::vector<int>(n, POS_INF));
 
         // For the src vertex, the dp value is always going to be 0
@@ -142,38 +146,38 @@ private:
 
         for (int i = 1; i < k; i++)
         {
-            // The prev_col will be dp[0] if i is odd (initially when i = 1
-            // dp[0] is the first column) and the values will be computed and stored in dp[1]
-            // And in the next iteration when i is even, dp[1] becomes the prev col
-            // and dp[0]'s values aren't obsolete, so we fill the current col's values in dp[0]
+            // The prev_row will be dp[0] if i is odd (initially when i = 1
+            // dp[0] is the first row) and the values will be computed and stored in dp[1]
+            // And in the next iteration when i is even, dp[1] becomes the prev row
+            // and dp[0]'s values aren't obsolete, so we fill the current row's values in dp[0]
             // And we keep alternating
 
-            // We use references to the original rows in the 2d dp array as prev_col and curr_col
-            // ! std::vector<int> &prev_col = dp[i % 2 == 1 ? 0 : 1];
-            // ! std::vector<int> &curr_col = dp[i % 2 == 1 ? 1 : 0];
+            // We use references to the original rows in the 2d dp array as prev_row and curr_row
+            // ! std::vector<int> &prev_row = dp[i % 2 == 1 ? 0 : 1];
+            // ! std::vector<int> &curr_row = dp[i % 2 == 1 ? 1 : 0];
             // ! `i % 2 == 1 ? 0 : 1` can be simplified to 1 - (i & 1)
             // ! `i % 2 == 1 ? 1 : 0` can be simplified to (i & 1)
-            std::vector<int> &prev_col = dp[1 - (i & 1)];
-            std::vector<int> &curr_col = dp[i & 1];
+            std::vector<int> &prev_row = dp[1 - (i & 1)];
+            std::vector<int> &curr_row = dp[i & 1];
             for (int v = 0; v < n; v++)
             {
                 if (v == src)
                     continue;
-                int &min_value = curr_col[v];
-                min_value = prev_col[v];
+                int &min_value = curr_row[v];
+                min_value = prev_row[v];
                 for (const auto &[u, cost] : reverse_adj_list[v])
-                    min_value = std::min(min_value, sum(prev_col[u], cost));
+                    min_value = std::min(min_value, sum(prev_row[u], cost));
             }
         }
 
-        // If k is odd, according to the invariant dp[0] will be the prev col
-        // and dp[1] will be the prev col if k is even
-        // Optimization: Entire kth col need not be computed, just the dst's index
+        // If k is odd, according to the invariant dp[0] will be the prev row
+        // and dp[1] will be the prev row if k is even
+        // Optimization: Entire kth row need not be computed, just the dst's index
         // value need to be computed.
-        std::vector<int> &prev_col = dp[1 - (k & 1)];
-        int min_value = prev_col[dst];
+        std::vector<int> &prev_row = dp[1 - (k & 1)];
+        int min_value = prev_row[dst];
         for (const auto &[u, cost] : reverse_adj_list[dst])
-            min_value = std::min(min_value, sum(prev_col[u], cost));
+            min_value = std::min(min_value, sum(prev_row[u], cost));
 
         // Return the answer
         return min_value == POS_INF
@@ -231,5 +235,5 @@ private:
 
 public:
     int findCheapestPrice(int n, const std::vector<std::vector<int>> &flights,
-                          int src, int dst, int k) { return sol2(n, flights, src, dst, k); }
+                          int src, int dst, int k) { return sol3(n, flights, src, dst, k); }
 };
