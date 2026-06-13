c204ccff6a297cde9bde8ed1033bd901640e4f4b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-13 15:20:36 +0530

    133: use vector instead of unordered_map

--- problems/0133-clone-graph/main.cpp
+++ problems/0133-clone-graph/main.cpp
@@ -116,6 +116,70 @@ private:
         return node_clone;
     }
 
+    /**
+     * Clones the graph node using BFS
+     * Same as solution 2 except we use a vector instead of an unordered_map
+     * to leverage the leetcode constraints:
+     * 1. The number of nodes in the graph is in the range [0, 100].
+     * 2. 1 <= node.val <= 100
+     * 3. node.val is unique for each node.
+     * So we can use a vector of size 101 instead of the unordered_map to speed up lookups
+     *
+     * Theta(V + E) time and Theta(V) space
+     */
+    static Node *sol3(Node *node)
+    {
+        // Base case
+        if (!node)
+            return nullptr;
+
+        // Invariants:
+        // 1. The nodes added to the queue have already been cloned and in the map
+        // 2. The neighbours of the nodes in the queue are empty
+
+        // Create the clone of node and add the node to the queue to start BFS
+        Node *node_clone = new Node(node->val);
+        std::vector<Node *> map(101, nullptr);
+        map[node->val] = node_clone;
+        std::queue<Node *> queue;
+        queue.push(node);
+
+        // BFS main loop
+        while (!queue.empty())
+        {
+            // Dequeue the node and clone & add its neighbours as the
+            // neighbours of the cloned node
+            Node *curr = queue.front();
+            queue.pop();
+
+            // Get the corresponding cloned node (exists in the map as per the invariant)
+            Node *curr_clone = map[curr->val];
+            curr_clone->neighbors.reserve(curr->neighbors.size());
+
+            // For each of the neighbours, if it was already cloned
+            // we can get it from the map and add it as the neighbour of curr_clone
+            // Otherwise we can create the clone and add it to the queue
+            for (Node *neighbor : curr->neighbors)
+            {
+                // If the neighbour wasn't already cloned, we clone it and add it to the map
+                // and push it to the queue as we cloned it for the first time
+                // ! We create a reference to prevent frequent indexing into the vector
+                Node *&neighbor_clone = map[neighbor->val];
+                if (!neighbor_clone)
+                {
+                    neighbor_clone = new Node(neighbor->val);
+                    queue.push(neighbor);
+                }
+
+                // Add the cloned neighbour as the neighbour of curr_clone
+                curr_clone->neighbors.push_back(neighbor_clone);
+            }
+        }
+
+        // Return the clone of the given node
+        return node_clone;
+    }
+
 public:
     Node *cloneGraph(Node *node) { return sol2(node); }
 };
