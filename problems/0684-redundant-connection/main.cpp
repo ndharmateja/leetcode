88d427f1e5de33500c3c7732295f3d26e391e090
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-13 10:24:30 +0530

    684: clean up disjoint sets

--- problems/0684-redundant-connection/main.cpp
+++ problems/0684-redundant-connection/main.cpp
@@ -3,7 +3,6 @@
 class DisjointSets
 {
 private:
-    int num_sets;
     std::vector<int> parent;
     std::vector<int> size;
 
@@ -17,7 +16,7 @@ private:
 
 public:
     // Initially all elements in size will just be 1
-    DisjointSets(int n) : num_sets{n}, size(n, 1)
+    DisjointSets(int n) : size(n, 1)
     {
         // Initialize the n components as initially all disconnected
         // Each element will have itself as its own parent
@@ -29,8 +28,11 @@ public:
             parent.push_back(i);
     }
 
-    // Weighted quick union
-    void connect(int p, int q)
+    /**
+     * Weighted quick union
+     * @return true if the two elements were not already connected, false otherwise
+     */
+    bool connect(int p, int q)
     {
         // Get the roots of each of the trees
         int p_root = root(p);
@@ -38,10 +40,7 @@ public:
 
         // If both of them belong to the same tree, they are already connected
         if (p_root == q_root)
-            return;
-
-        // Decrement the number of sets by 1 as we are merging two sets
-        num_sets--;
+            return false;
 
         // Attach the smaller size tree to the root of the larger size tree
         // and update the sizes
@@ -55,10 +54,8 @@ public:
             parent[q_root] = p_root;
             size[p_root] += size[q_root];
         }
+        return true;
     }
-
-    bool are_connected(int p, int q) { return root(p) == root(q); }
-    int get_num_sets() { return num_sets; }
 };
 
 class Solution
@@ -86,11 +83,8 @@ private:
             // disjoint sets data structure
             int from = edge.front() - 1;
             int to = edge.back() - 1;
-            if (ds.are_connected(from, to))
+            if (!ds.connect(from, to))
                 return edge;
-
-            // Otherwise connect the components
-            ds.connect(from, to);
         }
 
         // We would never reach here as it is guaranteed to have exactly n edges and n vertices
