fdc6b3246670f0514de28b0492604d6f3b6e5158
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-15 17:45:03 +0530

    78: fix typos and improve documentation

--- problems/0078-subsets/sol3.cpp
+++ problems/0078-subsets/sol3.cpp
@@ -6,15 +6,20 @@ private:
     /**
      * Finds all the subsets of the nums[idx:] and adds them to the result
      *
-     * Lets say S is the set of subsets that can be formed from nums[idx+1:]
-     * Then the subsets of nums[idx:] is the union of set S and S' which is
-     * the set formed by adding nums[i] to each of the elements of S
+     * S  := the set of subsets that can be formed from nums[idx+1:]
+     * S' := { x U {nums[idx]} for each x in S }
+     *
+     * S' is the set formed by adding nums[idx] to each of the elements of S
+     * Then the subsets of nums[idx:] = S U S'
      * S can be found recursively
      *
      * Eg: nums = [A, B, C]
-     * then S = subsets formed by nums[1:] = {{}, {B}, {C}, {B, C}}
-     * then S' = nums[i] added to each of the elements of S = {{A}, {A, B}, {A, C}, {A, B, C}}
-     * subsets formed by nums = S U S' = {{}, {B}, {C}, {B, C}, {A}, {A, B}, {A, C}, {A, B, C}}
+     * then S = subsets formed by nums[1:]
+     *        = {{}, {B}, {C}, {B, C}}
+     * then S' = (nums[idx]=nums[0]=A) added to each of the elements of S
+     *         = {{A}, {A, B}, {A, C}, {A, B, C}}
+     * subsets formed by nums = S U S'
+     *                        = {{}, {B}, {C}, {B, C}, {A}, {A, B}, {A, C}, {A, B, C}}
      */
     static void backtrack(const std::vector<int> &nums, const int idx, const int n,
                           std::vector<std::vector<int>> &result)
@@ -31,7 +36,7 @@ private:
         // Collect all the subsets formed by nums[idx+1:] in the result
         backtrack(nums, idx + 1, n, result);
 
-        // Add nums[i] to each of the elements in the result
+        // Add nums[idx] to each of the elements in the result
         // and add the new subsets
         // ! We can't do a for each loop as we are also modifying the result in the loop
         int result_size{static_cast<int>(result.size())};
