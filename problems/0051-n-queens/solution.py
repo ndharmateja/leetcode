4f1852cffdca538acef766fde2c9a0ca893fcd2a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-15 22:32:49 +0530

    51: update documentation

--- problems/0051-n-queens/solution.py
+++ problems/0051-n-queens/solution.py
@@ -8,7 +8,7 @@ class Solution:
             col_positions[row] = col
             col_visited[col] = True
             main_diagonal_visited[row - col + n - 1] = True
-            other_diagonal_visited[row + col] = True
+            anti_diagonal_visited[row + col] = True
 
             # If the row is n-1, it means that we have found a
             # valid placement of n-queens
@@ -22,31 +22,31 @@ class Solution:
                 for new_c in range(n):
                     if (not col_visited[new_c] 
                         and not main_diagonal_visited[new_r - new_c + n - 1]
-                        and not other_diagonal_visited[new_r + new_c]):
+                        and not anti_diagonal_visited[new_r + new_c]):
                         dfs(new_r, new_c)
 
             # Remove the queen at (row, col)
-            other_diagonal_visited[row + col] = False
+            anti_diagonal_visited[row + col] = False
             main_diagonal_visited[row - col + n - 1] = False
             col_visited[col] = False
             col_positions[row] = -1
     
-        # col_visited, main_diagonal_visited, other_diagonal_visited keep track
-        # of whether or not each col, main diagoanl and other diagonal is visited
-        # r-c value goes from -(n-1) to (n-1) for each of the main diagonals
+        # Lists col_visited, main_diagonal_visited, other_diagonal_visited keep track
+        # of whether or not each col, main diagoanl and anti diagonal is visited
+        # For each of the main diagonals, r-c value goes from -(n-1) to (n-1)
         # so (r-c)+(n-1) goes from 0 to (2n-1), so we can use (r-c)+(n-1) as an index
         # for each of the main diagonals
-        # r+c value goes from 0 to (2n-1) for each of the other diagonals
-        # so we can use r+c as an index for each of the other diagonals
+        # For each of the anti diagonals, r+c value goes from 0 to (2n-1)
+        # so we can use r+c as an index for each of the anti diagonals
         col_visited = [False for _ in range(n)]
         main_diagonal_visited = [False for _ in range(2*n - 1)]
-        other_diagonal_visited = [False for _ in range(2*n - 1)]
+        anti_diagonal_visited = [False for _ in range(2*n - 1)]
         col_positions = [-1 for _ in range(n)]
         result_col_positions = []
         for i in range(n):
             dfs(0, i)
 
-        # result_col_positions looks like [[1, 3, 0, 2], [2, 0, 3, 1]]
+        # If result_col_positions looks like [[1, 3, 0, 2], [2, 0, 3, 1]]
         # which means the solution is 
         # [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
         #
