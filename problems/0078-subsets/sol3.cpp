813a8a73f455f7b05eb814aebab40c78b608473c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-15 17:40:00 +0530

    78: add solution 3

---
+++ problems/0078-subsets/sol3.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+
+class Solution
+{
+private:
+    /**
+     * Finds all the subsets of the nums[idx:] and adds them to the result
+     *
+     * Lets say S is the set of subsets that can be formed from nums[idx+1:]
+     * Then the subsets of nums[idx:] is the union of set S and S' which is
+     * the set formed by adding nums[i] to each of the elements of S
+     * S can be found recursively
+     *
+     * Eg: nums = [A, B, C]
+     * then S = subsets formed by nums[1:] = {{}, {B}, {C}, {B, C}}
+     * then S' = nums[i] added to each of the elements of S = {{A}, {A, B}, {A, C}, {A, B, C}}
+     * subsets formed by nums = S U S' = {{}, {B}, {C}, {B, C}, {A}, {A, B}, {A, C}, {A, B, C}}
+     */
+    static void backtrack(const std::vector<int> &nums, const int idx, const int n,
+                          std::vector<std::vector<int>> &result)
+    {
+        // Base case:
+        // when idx is n, then all the subsets formed by nums[n:] = {{}}
+        // is just the set of empty set as the empty set is the only subset
+        if (idx == n)
+        {
+            result.push_back(std::vector<int>());
+            return;
+        }
+
+        // Collect all the subsets formed by nums[idx+1:] in the result
+        backtrack(nums, idx + 1, n, result);
+
+        // Add nums[i] to each of the elements in the result
+        // and add the new subsets
+        // ! We can't do a for each loop as we are also modifying the result in the loop
+        int result_size{static_cast<int>(result.size())};
+        for (int i = 0; i < result_size; i++)
+        {
+            result.push_back(result[i]);
+            result.back().push_back(nums[idx]);
+        }
+    };
+
+    static std::vector<std::vector<int>> sol(const std::vector<int> &nums)
+    {
+        int n{static_cast<int>(nums.size())};
+        std::vector<std::vector<int>> result;
+        result.reserve(1 << n);
+        backtrack(nums, 0, n, result);
+        return result;
+    };
+
+public:
+    std::vector<std::vector<int>> subsets(const std::vector<int> &nums) { return sol(nums); };
+};
