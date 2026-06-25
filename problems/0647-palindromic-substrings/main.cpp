2c208499c6439fa2f443470d076698e9349ad621
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-25 22:07:22 +0530

    647: strip down Manacher's class

--- problems/0647-palindromic-substrings/main.cpp
+++ problems/0647-palindromic-substrings/main.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 
 /**
+ * Stripped down version of the full class
  * For full details, see the following reference:
  * ref: https://github.com/ndharmateja/data-structures-cpp/blob/master/algorithms/manachers/manachers.hpp
  */
@@ -36,10 +37,9 @@ class Manachers
 
 private:
     static inline const char DELIMITER = '#';
-    int str_len, aug_len;
+    int aug_len;
     std::vector<int> radii;
-    std::string str, aug;
-    int longest_palindrome_len{0}, longest_palindrome_center{0};
+    std::string aug;
 
     void run_manachers()
     {
@@ -61,15 +61,6 @@ private:
                    aug[i - 1 - curr_radius] == aug[i + 1 + curr_radius])
                 curr_radius++;
 
-            // Update the max palindrome length
-            // Note that the radius represents the palindrome length centered here
-            // (read the explanation above for more details)
-            if (curr_radius > longest_palindrome_len)
-            {
-                longest_palindrome_len = curr_radius;
-                longest_palindrome_center = i;
-            }
-
             // Step 4: The Handoff (Update the Active Center)
             // If this palindrome pushed the frontier further right, it dethrones the old center.
             if (i + curr_radius > active_right_boundary)
@@ -81,8 +72,7 @@ private:
     }
 
 public:
-    Manachers(const std::string &input) : str_len{static_cast<int>(input.size())},
-                                          aug_len{2 * str_len + 1}, radii(aug_len), str{input}
+    Manachers(const std::string &input) : aug_len{2 * static_cast<int>(input.size()) + 1}, radii(aug_len)
     {
         // Create the augmented string with the delimiter (default is #)
         // to be able to detect even lengthed palindromes
@@ -96,60 +86,6 @@ public:
 
         // Run Manacher's algorithm which will update the radii vector
         run_manachers();
-    }
-
-    /**
-     * Returns if the substring in the range [start, end) is a palindrome
-     * start is inclusive but end is exclusive
-     */
-    bool is_palindrome(int start, int end) const
-    {
-        // Check out of bounds and invalid bounds
-        if (start < 0 || end > str_len || start > end)
-            return false;
-
-        // If start == end or start == end - 1
-        // it means we are looking at 0 and 1 lengthed substrings
-        // which will always be palindromes
-        if (start == end || start == end - 1)
-            return true;
-
-        // [start, end-1] is the range we are looking for
-        // corresponding indices in the augmented string = [2*start + 1, 2*(end-1) + 1]
-        // Center in the augmented string
-        // = [(2start + 1) + (2(end-1) + 1)] / 2 = start + (end-1) + 1 = start + end
-        // int substr_len{end - start};
-        // int center = {start + end};
-
-        // The radius at this center tells us the length of the palindrome centered here
-        // so if this length is greater than or equal to the substring length
-        // then it is a palindrome
-        // return radii[center] >= substr_len
-        return radii[start + end] >= (end - start);
-    }
-
-    /**
-     * Gets the length of the longest palindrome contained within the given input string
-     * Runs in Theta(1) time
-     */
-    int get_longest_palindrome_length() const { return longest_palindrome_len; }
-
-    /**
-     * Gets the longest palindrome contained within the given input string (returns the
-     * leftmost one if there are ties)
-     * Runs in Theta(1) time
-     */
-    std::string get_longest_palindrome() const
-    {
-        //           3     4     5     6     7     8     9     10    11
-        //           ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓
-        // Eg:    #  A  #  I  #  B  #  I  #  O  #  I  #  B  #  I  #  A  #
-        // radii: 1  3  0  1  0  3  0  1  0  9  0  1  0  3  0  1  0  1  0
-        //        ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑  ↑
-        //        6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
-        // longest_palindrome_center is 15 and the start index in the string is (15-9)/2 = 3
-        int start_index = (longest_palindrome_center - longest_palindrome_len) / 2;
-        return str.substr(start_index, longest_palindrome_len);
     }
 
     /**
