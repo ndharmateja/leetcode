269186f94fed2cf0062a01e0d0a2dc40a7bc09a3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-07-21 07:31:21 +0530

    424: add Theta(n) solutions in C++

--- problems/0424-longest-repeating-character-replacement/main.cpp
+++ problems/0424-longest-repeating-character-replacement/main.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
 #include <array>
 #include <string>
-#include <algorithm>
+#include <unordered_map>
+#include <bitset>
+#include <vector>
 
 class Solution
 {
@@ -40,6 +43,117 @@ class Solution
         return global_max;
     }
 
+    /**
+     * Same as solution 1, except we maintain the max count as we go through
+     * different windows instead of spending Theta(26) time to find it for each
+     * sliding window
+     */
+    static int sol2(const std::string &s, int k)
+    {
+        int start{0}, end{0}, n{static_cast<int>(s.size())};
+        int global_max{0}, max_char_count{0};
+        int new_count;
+        int c;
+
+        std::array<int, 26> char_counts{};
+        std::unordered_map<int, std::bitset<26>> count_to_char_map;
+        count_to_char_map.reserve(n);
+        count_to_char_map[0].set();
+
+        // Invariant : At any point, the window has the property that we could form the
+        // result with the window length by replacing <= k chars
+
+        while (end < n)
+        {
+            // Add char at end to the window
+            c = s[end++] - 'A';
+            new_count = ++char_counts[c];
+            count_to_char_map[new_count - 1].reset(c);
+            count_to_char_map[new_count].set(c);
+            max_char_count = std::max(max_char_count, new_count);
+
+            // If the invariant still holds we can record this window length and move to the next one
+            if (max_char_count >= (end - start) - k)
+            {
+                global_max = std::max(global_max, end - start);
+                continue;
+            }
+
+            // If the invariant is broken, remove characters from front until the invariant is satisfied
+            while (true)
+            {
+                c = s[start++] - 'A';
+                new_count = --char_counts[c];
+                count_to_char_map[new_count + 1].reset(c);
+                count_to_char_map[new_count].set(c);
+
+                if (count_to_char_map[max_char_count].none())
+                    max_char_count = new_count;
+
+                if (max_char_count >= (end - start) - k)
+                    break;
+            }
+            global_max = std::max(global_max, end - start);
+        }
+
+        return global_max;
+    }
+
+    /**
+     * Same as solution 2, except we use a vector instead of an unordered_map
+     * and also only track number of characters that occur at each count
+     * instead of tracking all the characters that occur at each count
+     */
+    static int sol3(const std::string &s, int k)
+    {
+        int start{0}, end{0}, n{static_cast<int>(s.size())};
+        int global_max{0}, max_char_count{0};
+        int new_count;
+        int c;
+
+        std::array<int, 26> char_counts{};
+        std::vector<char> freq_of_freq(n + 1);
+        freq_of_freq[0] = 26;
+
+        // Invariant : At any point, the window has the property that we could form the
+        // result with the window length by replacing <= k chars
+
+        while (end < n)
+        {
+            // Add char at end to the window
+            c = s[end++] - 'A';
+            new_count = ++char_counts[c];
+            freq_of_freq[new_count - 1]--;
+            freq_of_freq[new_count]++;
+            max_char_count = std::max(max_char_count, new_count);
+
+            // If the invariant still holds we can record this window length and move to the next one
+            if (max_char_count >= (end - start) - k)
+            {
+                global_max = std::max(global_max, end - start);
+                continue;
+            }
+
+            // If the invariant is broken, remove characters from front until the invariant is satisfied
+            while (true)
+            {
+                c = s[start++] - 'A';
+                new_count = --char_counts[c];
+                freq_of_freq[new_count + 1]--;
+                freq_of_freq[new_count]++;
+
+                if (!freq_of_freq[max_char_count])
+                    max_char_count = new_count;
+
+                if (max_char_count >= (end - start) - k)
+                    break;
+            }
+            global_max = std::max(global_max, end - start);
+        }
+
+        return global_max;
+    }
+
 public:
-    int characterReplacement(const std::string &s, int k) { return sol1(s, k); }
+    int characterReplacement(const std::string &s, int k) { return sol3(s, k); }
 };
